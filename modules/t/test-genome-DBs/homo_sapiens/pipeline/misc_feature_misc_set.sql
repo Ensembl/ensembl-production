ced7a9bfb18afac3bbb8a7d6ad75c9d228279ec6
Kieron Taylor <ktaylor@ebi.ac.uk>
2013-05-22 10:36:12 +0000

    Removing misleadingly named database files.

--- modules/t/test-genome-DBs/homo_sapiens/pipeline/misc_feature_misc_set.sql
+++
@@ -1,9 +0,0 @@
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `misc_feature_misc_set` (
-  `misc_feature_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `misc_set_id` smallint(5) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`misc_feature_id`,`misc_set_id`),
-  KEY `reverse_idx` (`misc_set_id`,`misc_feature_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
--- modules/t/test-genome-DBs/homo_sapiens/pipeline/seq_region_mapping.sql
+++
@@ -1,9 +0,0 @@
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `seq_region_mapping` (
-  `external_seq_region_id` int(10) unsigned NOT NULL,
-  `internal_seq_region_id` int(10) unsigned NOT NULL,
-  `mapping_set_id` int(10) unsigned NOT NULL,
-  KEY `mapping_set_idx` (`mapping_set_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
