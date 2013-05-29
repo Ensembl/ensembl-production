6cb32e676caa89176862e51da448175bcc4b9222
Alessandro Vullo <avullo@ebi.ac.uk>
2013-05-29 08:35:10 +0000

    Check species parameter

--- modules/Bio/EnsEMBL/Production/Pipeline/GTF/Base.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/GTF/Base.pm
@@ -6,6 +6,10 @@ use base qw/Bio::EnsEMBL::Production::Pipeline::Base/;
 
 sub data_path {
   my ($self) = @_;
+
+  $self->throw("No 'species' parameter specified") 
+    unless $self->param('species');
+
   return $self->get_dir('gtf', $self->param('species'));
 }
 
