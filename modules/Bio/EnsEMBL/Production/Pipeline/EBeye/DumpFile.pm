93a01443ea37d2c968a3bd808e9a615f1b889aa5
Alessandro Vullo <avullo@ebi.ac.uk>
2013-06-13 15:11:09 +0000

    Private method to retrieve orthologs

--- modules/Bio/EnsEMBL/Production/Pipeline/EBeye/DumpFile.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/EBeye/DumpFile.pm
@@ -2,7 +2,7 @@
 
 =head1 LICENSE
 
-  Copyright (c) 1999-2012 The European Bioinformatics Institute and
+  Copyright (c) 1999-2013 The European Bioinformatics Institute and
   Genome Research Limited.  All rights reserved.
 
   This software is distributed under a modified Apache license.
@@ -20,15 +20,15 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::Production::Pipeline::Flatfile::DumpFile
+Bio::EnsEMBL::Production::Pipeline::EBeye::DumpFile
 
 =head1 DESCRIPTION
 
-The main workhorse of the Flatfile dumping pipeline.
+The main workhorse of the EBeye dumping pipeline.
 
 The script is responsible for creating the filenames of these target
-files, taking data from the database and the formatting of the flat files
-headers. The final files are all Gzipped at normal levels of compression.
+files, taking data from the database and the formatting of the XML files.
+The final files are all Gzipped at normal levels of compression.
 
 Allowed parameters are:
 
@@ -40,18 +40,18 @@ Allowed parameters are:
 
 =item release - The current release we are emitting
 
-=item type - The type of data we are emitting. Should be embl or genbank
+=item type - The database type of data we are emitting. Should be core or vega
 
 =back
 
 =cut
 
-package Bio::EnsEMBL::Production::Pipeline::Flatfile::DumpFile;
+package Bio::EnsEMBL::Production::Pipeline::EBeye::DumpFile;
 
 use strict;
 use warnings;
 
-use base qw(Bio::EnsEMBL::Production::Pipeline::Flatfile::Base);
+use base qw(Bio::EnsEMBL::Production::Pipeline::EBeye::Base);
 
 use Bio::EnsEMBL::Utils::Exception qw/throw/;
 use Bio::EnsEMBL::Utils::SeqDumper;
@@ -61,7 +61,7 @@ use File::Path qw/rmtree/;
 sub param_defaults {
   my ($self) = @_;
   return {
-    supported_types => {embl => 1, genbank => 1},
+    supported_types => { core => 1, vega => 1 },
   };
 }
 
@@ -82,69 +82,90 @@ sub fetch_input {
 sub run {
   my ($self) = @_;
   
-  my $root = $self->data_path();
-  if(-d $root) {
-    $self->info('Directory "%s" already exists; removing', $root);
-    rmtree($root);
-  }
-  
+  my ($want_species_orthologs, $ortholog_lookup) =
+    $self->_fetch_orthologs;
+
   my $type = $self->param('type');
-  my $target = "dump_${type}";
-  my $seq_dumper = $self->_seq_dumper();
-  
-  my @chromosomes;
-  my @non_chromosomes;
-  foreach my $s (@{$self->get_Slices()}) {
-    my $chr = $s->is_chromosome();
-    push(@chromosomes, $s) if $chr;
-    push(@non_chromosomes, $s) if ! $chr;
-  }
-  
-  if(@non_chromosomes) {
-    my $path = $self->_generate_file_name('nonchromosomal');
-    $self->info('Dumping non-chromosomal data to %s', $path);
-    gz_work_with_file($path, 'w', sub {
-      my ($fh) = @_;
-      foreach my $slice (@non_chromosomes) {
-        $self->fine('Dumping non-chromosomal %s', $slice->name());
-        $seq_dumper->$target($slice, $fh);
-      }
-      return;
-    });
-  }
-  else {
-    $self->info('Did not find any non-chromosomal data');
-  }
-  
-  foreach my $slice (@chromosomes) {
-    $self->fine('Dumping chromosome %s', $slice->name());
-    my $path = $self->_generate_file_name($slice->coord_system_name(), $slice->seq_region_name());
-    my $args = {};
-    if(-f $path) {
-      $self->fine('Path "%s" already exists; appending', $path);
-      $args->{Append} = 1;
-    }
-    gz_work_with_file($path, 'w', sub {
-      my ($fh) = @_;
-      $seq_dumper->$target($slice, $fh);
-      return;
-    }, $args);
-  }
-  
-  $self->_create_README();
-  
+  my $dba = $self->get_DBAdaptor($type);
+
   return;
 }
 
-sub _seq_dumper {
-  my ($self) = @_;
-  my $seq_dumper = Bio::EnsEMBL::Utils::SeqDumper->new();
-  $seq_dumper->disable_feature_type('similarity');
-  $seq_dumper->disable_feature_type('genscan');
-  $seq_dumper->disable_feature_type('variation');
-  $seq_dumper->disable_feature_type('repeat');
-  return $seq_dumper;
+sub _fetch_orthologs {
+  my $self = shift;
+
+  my $orth_species = {
+		      'homo_sapiens'             => 'ensembl_ortholog',
+		      'mus_musculus'             => 'ensembl_ortholog',
+		      'drosophila_melanogaster'  => 'ensemblgenomes_ortholog',
+		      'caenorhabditis_elegans'   => 'ensemblgenomes_ortholog',
+		      'saccharomyces_cerevisiae' => 'ensemblgenomes_ortholog'
+		     };
+
+  my $want_species_orthologs;
+  my $ortholog_lookup;
+  my $species = $self->param('species');
+  my $orth_target_species;
+  ( $orth_target_species = lcfirst($species) ) =~ s/\s/_/;
+
+  if ( $want_species_orthologs =
+       delete( $orth_species->{$orth_target_species} ) ) {
+
+    $self->info("Fetching orthologs [%s]", $species);
+
+    my $dbc = Bio::EnsEMBL::Registry->get_DBAdaptor('Multi', 'compara')->dbc();
+    defined $dbc or die "Unable to get Compara DBConnection";
+
+    my @wanted_ortholog_species = keys %$orth_species;
+
+    #  change perl array value interpolation
+    local $" = qq{","};
+    my $orth_species_string = qq/@wanted_ortholog_species/;
+
+    # Get orthologs
+    my $sth = $dbc->prepare(qq{SELECT m1.stable_id , m2.stable_id, gdb2.name
+                               FROM genome_db gdb1 JOIN member m1 USING (genome_db_id) 
+                                    JOIN homology_member hm1 USING (member_id)
+                                    JOIN homology h USING (homology_id)
+                                    JOIN homology_member hm2 USING (homology_id)
+                                    JOIN member m2 ON (hm2.member_id = m2.member_id)
+                                    JOIN genome_db gdb2 on (m2.genome_db_id = gdb2.genome_db_id)
+                               WHERE gdb1.name = "$orth_target_species" 
+                                     AND m2.source_name = "ENSEMBLGENE"
+                                     AND gdb2.name IN ("$orth_species_string")
+                                     AND h.description in ("ortholog_one2one", 
+                                                           "apparent_ortholog_one2one", 
+                                                           "ortholog_one2many", 
+                                                           "ortholog_many2many")}
+			   );
+
+    $sth->execute;
+    my $rows = []; # cache for batches of rows
+    while (
+	   my $row = (
+		      shift(@$rows) || # get row from cache, or reload cache:
+		      shift(
+			    @{
+			      $rows =
+				$sth->fetchall_arrayref( undef, 10_000 )
+				  || []
+				}
+			   )
+		     )
+          ) {
+      push @{ $ortholog_lookup->{ $row->[0] } },
+	[ $row->[1], $orth_species->{ $row->[2] } ];
+
+    }
+    $sth->finish;
+
+    $self->info("Done Fetching Orthologs");
+  }
+
+  return ($want_species_orthologs, );
 }
+
+
 
 sub _generate_file_name {
   my ($self, $section, $name) = @_;
@@ -163,90 +184,10 @@ sub _generate_file_name {
 
   my $file_name = join( '.', @name_bits );
   my $path = $self->data_path();
+
   return File::Spec->catfile($path, $file_name);
 }
 
-sub _create_README {
-  my ($self) = @_;
-  my $species = $self->scientific_name();
-  my $format = uc($self->param('type'));
-  
-  my $readme = <<README;
-#### README ####
-
-IMPORTANT: Please note you can download correlation data tables, 
-supported by Ensembl, via the highly customisable BioMart and 
-EnsMart data mining tools. See http://www.ensembl.org/biomart/martview or
-http://www.ebi.ac.uk/biomart/ for more information.
-
------------------------
-$format FLATFILE DUMPS
------------------------
-This directory contains $species $format flatfile dumps.  To ease 
-downloading of the files, the $format format entries are bundled 
-into groups of chromosomes and non-chromosomal regions.  
-All files are then compacted with gzip.
-
-Ensembl provides an automatic reannotation of $species genomic data.
-These data will be dumped in a number of forms - one of them being 
-$format flat files.  As the annotation of this form comes from Ensembl, 
-and not the original sequence entry, the two annotations are 
-likely to be different.
-
-$format flat file format dumping provides all the confirmed protein coding 
-genes known by Ensembl. Considerably more information is stored in Ensembl: 
-the flat file just gives a representation which is compatible with 
-existing tools.
-
-The main body of the entry gives the same information as is in the main 
-$format flat file entry.
-
-    * ID - the $format id
-    * AC - the EMBL/GenBank/DDBJ accession number (only the primary 
-           accession number used)
-    * SV - The accession.version pair which gives the exact reference to 
-           a particular sequence
-    * CC - comment lines to help you interpret the entry 
-
-Currently the following features are dumped into the feature table of 
-the Ensembl entry:
-
-    * Transcripts as CDS entries. Each transcript has the following 
-      attributes attached
-          o Transcript id - a stable id, which Ensembl will attempt to 
-            preserve as sensibly as possible during updates of the data
-          o Gene id - indication of the gene that this transcript belongs 
-            to. gene ids are stable and preserved as sensibly as possible 
-            during updates of the data
-          o Translation - the peptide translation of the transcript. 
-    * Exons as exon entries. Each exon has the following information
-          o Exon id. The exon id is stable and preserved as sensibly 
-            as possible during sequence updates
-          o start_phase. The phase of the splice site at the 5' end 
-            of the exon. Phase 0 means between two codons, phase 1 
-            means between the first and the second base of the codon 
-            (meaning that there are 2 bases until the reading frame of 
-            the exon) and phase 2 means between the second and the third 
-            base of the codon (one base until the reading frame starts).
-          o end_phase. The phase of the splice site at the 3' end of the 
-            exon: same definition as above (though of course, being end_phase, 
-            the position relative to the exon's reading frame is different 
-            for phase 1 and 2). 
-
-We are considering other information that should be made dumpable. In 
-general we would prefer people to use database access over flat file 
-access if you want to do something serious with the data. 
-
-README
-  
-  my $path = File::Spec->catfile($self->data_path(), 'README');
-  work_with_file($path, 'w', sub {
-    my ($fh) = @_;
-    print $fh $readme;
-    return;
-  });
-  return;
-}
 
 
 1;
