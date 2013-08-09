4d4b0a1dac2b14dea221c8bb94a806d0a8faca72
Magali Ruffier <mr6@ebi.ac.uk>
2013-08-09 11:48:34 +0000

    cosmetic changes

--- sql/patch_72_73b.sql
+++ sql/patch_72_73b.sql
@@ -1,7 +1,7 @@
-alter table species add unique index species_prefix_idx(species_prefix) ;
-alter table species add unique index taxon_idx(taxon) ;
-alter table species add unique index production_name_idx(production_name) ;
-alter table species modify column species_prefix varchar(7) not null ;
+ALTER TABLE species ADD UNIQUE INDEX species_prefix_idx(species_prefix) ;
+ALTER TABLE species ADD UNIQUE INDEX taxon_idx(taxon) ;
+ALTER TABLE species ADD UNIQUE INDEX production_name_idx(production_name) ;
+ALTER TABLE species MODIFY COLUMN species_prefix varchar(7) not null ;
 
 -- Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
