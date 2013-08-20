87e949e591b6dfa06da82cb47f273b5eee9f1f99
Magali Ruffier <mr6@ebi.ac.uk>
2013-08-20 13:04:57 +0000

    removing sql files for each table we only need one general table.sql file to create all tables, and .txt files for each table for content

--- modules/t/test-genome-DBs/homo_sapiens/core/meta_coord.sql
+++
@@ -1,9 +0,0 @@
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `meta_coord` (
-  `table_name` varchar(40) NOT NULL,
-  `coord_system_id` int(10) unsigned NOT NULL,
-  `max_length` int(11) DEFAULT NULL,
-  UNIQUE KEY `cs_table_name_idx` (`coord_system_id`,`table_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
--- modules/t/test-genome-DBs/homo_sapiens/core/unmapped_reason.sql
+++
@@ -1,9 +0,0 @@
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `unmapped_reason` (
-  `unmapped_reason_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `summary_description` varchar(255) DEFAULT NULL,
-  `full_description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`unmapped_reason_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=139 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
