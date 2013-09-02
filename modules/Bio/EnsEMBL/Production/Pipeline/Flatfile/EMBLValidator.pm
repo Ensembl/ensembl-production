b3b46cca79d798987dca83f625f584c62ff34f10
Alessandro Vullo <avullo@ebi.ac.uk>
2013-09-02 11:19:32 +0000

    Implements a validator for EMBL files

---
+++ modules/Bio/EnsEMBL/Production/Pipeline/Flatfile/EMBLValidator.pm
@@ -0,0 +1,173 @@
+=pod
+
+=head1 LICENSE
+
+  Copyright (c) 1999-2013 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <dev@ensembl.org>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+=head1 NAME
+
+Bio::EnsEMBL::Production::Pipeline::Flatfile::EMBLValidator
+
+=head1 DESCRIPTION
+
+Implements a validator for EMBL files.
+
+=back
+
+=cut
+
+package Bio::EnsEMBL::Production::Pipeline::Flatfile::EMBLValidator;
+
+use strict;
+use warnings;
+use Readonly;
+
+use base qw(Bio::EnsEMBL::Production::Pipeline::Flatfile::Validator);
+use Bio::EnsEMBL::Utils::Exception qw/throw/;
+
+Readonly my $RECORD_SEPARATOR => "//\n";
+
+sub _parse {
+  my $self = shift;
+
+  my $line = $self->_readline;
+  # This needs to be before the first eof() test
+ 
+  return unless defined $line; # no throws - end of file
+ 
+  if ( $line =~ /^\s+$/ ) {
+    while ( defined ($line = $self->_readline) ) {
+      $line =~/^\S/ && last;
+    }
+    # return without error if the whole next sequence was just a single
+    # blank line and then eof
+    return unless $line;
+  }
+ 
+  # no ID as 1st non-blank line, need short circuit and exit routine
+  throw "EMBL entry with no ID"
+    unless $line =~ /^ID\s+\S+/;
+ 
+  throw "Unrecognized format for EMBL ID header line:\n\n$line\n"
+    unless $line =~ 
+      m/^ID\s{3}(\w+);\s+SV (\d+); (\w+); ([^;]+); (\w{3}); (\w{3}); (\d+) BP.|^ID\s{3}(\S+)[^;]*;\s+(\S+)[^;]*;\s+(\S+)[^;]*;/;
+  my ($name, $sv, $topology, $mol, $div) = ($1, $2, $3, $4, $6);
+  # TODO
+  # check the above attributes
+ 
+  my $buffer = $self->_readline;
+
+  local $_;
+
+  my $end_of_entry = 0;
+
+  until ( !defined $buffer ) {
+    $_ = $buffer;
+
+    m{^//} and do { $end_of_entry = 1; } and last;
+
+    /^(AC|PR|D[TER]|KW|O[SCG]|R[NCPXGATL]|CC|A[HS]|F[HT]|XX|S[QV]|CO)/
+      or throw "Unrecognized code in line: $_";
+
+    if (/^SQ/) {
+      /^SQ\s{3}Sequence\s\d+\sBP;\s\d+\s[ACGT];\s\d+\s[ACGT];\s\d+\s[ACGT];\s\d+\s[ACGT];/
+	or throw "Invalid format for sequence header: $_";
+
+      while ( defined ($_ = $self->_readline) ) {
+	m{^//} and do { $self->_pushback($_); } and last;
+	# check the sequence
+	/^\s{5}([ACGT]{10}\s+){6}\s+\d+?$|^\s{5}([ACGT]+\s)+\s+\d+?$/ or
+	  throw "Invalid sequence line:\n\n$_";
+      }
+    }
+
+    /^FT\s{3}(\S+)\s+(\S+)|^FT\s{19}\S+/ or throw "Invalid feature line: $_"
+      if /^FT/;
+
+    # accession number
+    if (/^(AC|PA)/) { 
+      if ( /^..\s{3}(.*)?/) {
+	my @accs = split(/[; ]+/, $1); # allow space in addition
+	throw "Cannot get accession numbers from line: $_" unless @accs;
+      } else {
+	throw "Invalid AC/PA line: $_";
+      }
+    } 
+
+    # project identifier
+    /^PR\s{3}\S/ or throw "Invalid PR line: $_"
+      if /^PR/;
+    
+    # date
+    if (/^DT/) {
+      if ( /^DT\s{3}(.+)$/ ) {
+	my $line = $1;
+	my ($date, $version) = split(' ', $line, 2);
+	
+	$date =~ tr/,//d; # remove comma if new version
+	throw "Invalid date: $_"
+	  unless $date =~ /^\d{2}-[A-Z]{3}-\d{4}/;
+
+      } else {
+	throw "Invalid date format: $_";
+      }
+    }
+   
+    # description line(s)
+    /^DE\s{3}\w/ or throw "Invalid DE line: $_"
+      if /^DE/;
+
+    # keywords
+    /^KW\s{3}\S/ or throw "Invalid KW line: $_"
+      if /^KW/;
+    
+    # organism name and phylogenetic information
+    if (/^O[SCG]/) {
+      /^O[SCG]\s{3}(.+)/;
+      throw "Invalid organism/phylogenetic information on line: $_"
+	unless $1;
+    }
+ 
+    # references
+    /^R[NCPXGATL]\s{3}\S/ or throw "Invalid reference line: $_"
+      if /^R/;
+ 
+    # DB Xrefs
+    if(/^DR/) {
+      if (/^DR\s{3}([^\s;]+);\s*([^\s;]+);?\s*([^\s;]+)?\.$/) {
+	throw "Missing database/primary identifier in line: $_"
+	  unless $1 and $2;
+      } else {
+	throw "Invalid cross-reference: $_";
+      }
+    }
+ 
+    # comments
+    /^CC\s{3}\S/ or throw "Invalid comment line: $_"
+      if /^CC/;
+
+    $buffer = $self->_readline;
+  }
+ 
+  throw "Missing end-of-entry code"
+    unless $end_of_entry;
+
+  return 1;
+}
+
+
+1;
