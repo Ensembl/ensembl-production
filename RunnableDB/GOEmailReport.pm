af6222013e9f5f03fc03a7114ba71aa07bad0dde
ChuangKee Ong <ckong@ebi.ac.uk>
2015-01-22 14:10:25 +0000

    Add summary report based on analysis_type

--- RunnableDB/GOEmailReport.pm
+++ RunnableDB/GOEmailReport.pm
@@ -47,6 +47,7 @@ sub fetch_input {
        my $dbh   = $ga->dbc()->db_handle();
        $reports .= $self->linkage_type_summary($dbh, $ga, $sp);
        $reports .= $self->info_type_summary($dbh, $ga, $sp);
+       $reports .= $self->analysis_type_summary($dbh, $ga, $sp); 
     }
     $reports   .= "\n$subject detail log file is available at $output_dir.\n";
 
@@ -89,6 +90,28 @@ sub info_type_summary {
 
     my $from_species = $self->param_required('source');
     my $title        = "Summary of GO annotations, by info types: (projected from $from_species to $sp)";
+    my $columns      = $sth->{NAME};
+    my $results      = $sth->fetchall_arrayref();
+
+return $self->format_table($title, $columns, $results);
+}
+
+sub analysis_type_summary {
+    my ($self, $dbh, $ga, $sp) = @_;
+
+    my $sql = 'SELECT a.logic_name,count(dbprimary_acc) as count_of_GO_accession 
+                FROM xref x 
+                JOIN object_xref ox USING (xref_id) 
+                JOIN ontology_xref onx USING (object_xref_id) 
+		JOIN analysis a USING (analysis_id)
+                WHERE x.external_db_id=1000 
+                GROUP BY ox.analysis_id';
+
+    my $sth = $dbh->prepare($sql);
+    $sth->execute();
+
+    my $from_species = $self->param_required('source');
+    my $title        = "Summary of GO annotations, by analysis types: (projected from $from_species to $sp)";
     my $columns      = $sth->{NAME};
     my $results      = $sth->fetchall_arrayref();
 
