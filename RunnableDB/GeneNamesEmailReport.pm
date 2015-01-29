2994443ae3b0acfccf65cee0d0fb27890ca581c7
ChuangKee Ong <ckong@ebi.ac.uk>
2015-01-29 13:28:53 +0000

    code cleanup

--- RunnableDB/GeneNamesEmailReport.pm
+++ RunnableDB/GeneNamesEmailReport.pm
@@ -36,26 +36,37 @@ use base ('Bio::EnsEMBL::EGPipeline::Common::RunnableDB::EmailReport');
 sub fetch_input {
     my ($self) = @_;
 
-    my $email      = $self->param('email')   || die "'email' parameter is obligatory";
-    my $subject    = $self->param('subject') || "An automatic message from your pipeline";
-    my $output_dir = $self->param('output_dir');
-    my $species    = $self->param_required('species');
+    my $email                  = $self->param_required('email');
+    my $subject                = $self->param('subject') || "An automatic message from your pipeline";
+    my $output_dir             = $self->param('output_dir');
+    my $flag_store_projections = $self->param_required('flag_store_projections');
+    my $flag_GeneDesc          = $self->param_required('flag_GeneDesc');
+    my $species                = $self->param_required('species');
+
     my $reports;
 
     foreach my $sp (@$species){
-       my $ga    = Bio::EnsEMBL::Registry->get_adaptor($sp, 'core', 'Gene');
-       my $dbh   = $ga->dbc()->db_handle();
-       $reports   .= $self->info_type_summary($dbh, $ga, $sp);
-       $reports   .= $self->geneDesc_summary($dbh, $ga, $sp);
-    }
+       my $dba   = Bio::EnsEMBL::Registry->get_adaptor($sp, 'core', 'Gene');
+       my $dbh   = $dba->dbc()->db_handle();
 
-    $reports   .= "\n$subject detail log file is available at $output_dir.\n";
+       if(!$flag_store_projections){
+	  $reports .= "Pipeline was run with a flag to prevent storing anything in the database.\n";
+       }
+       else {
+	 if(!$flag_GeneDesc){
+	    $reports .= $self->info_type_summary($dbh, $sp);
+	 }
+	    $reports .= $self->geneDesc_summary($dbh, $sp);
+      }
+   }
 
-    $self->param('text', $reports);
+   $reports   .= "\n$subject detail log file is available at $output_dir.\n";
+
+   $self->param('text', $reports);
 }
 
 sub info_type_summary {
-    my ($self, $dbh, $ga, $sp) = @_;
+    my ($self, $dbh, $sp) = @_;
 
     my $sql = 'SELECT info_type,count(dbprimary_acc) as count_of_primary_acc 
                 FROM gene g INNER JOIN xref x
@@ -73,14 +84,17 @@ return $self->format_table($title, $columns, $results);
 }
 
 sub geneDesc_summary {
-    my ($self, $dbh, $ga, $sp) = @_;
+    my ($self, $dbh, $sp) = @_;
 
-    my $sql = 'SELECT count(*) FROM gene WHERE description rlike "project"';
+
+    my $sql = 'SELECT count(*) AS Total FROM gene WHERE description rlike "projected" OR description rlike "Projected"';
 
     my $sth = $dbh->prepare($sql);
     $sth->execute();
 
-    my $title = "Count of genes with projected gene description: ($sp)";
+    my $from_species = $self->param_required('source');
+
+    my $title = "Count of genes with projected gene description: (from $from_species to $sp)";
     my $columns = $sth->{NAME};
     my $results = $sth->fetchall_arrayref();
 
