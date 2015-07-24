53eae990c14cd5e1fe0497e4deb5744c93cbc272
James Allen <jallen@ebi.ac.uk>
2015-07-24 10:09:05 +0100

    Changes to make db dumping code compatible with Hive 2.3

--- DatabaseDumper.pm
+++ DatabaseDumper.pm
@@ -91,4 +91,85 @@ sub fetch_input {
   
 }
 
+sub run {
+  my $self = shift @_;
+
+  my $src_dbc = $self->param('src_dbc');
+  my $tables = $self->param('tables');
+  my $ignores = $self->param('ignores');
+
+  # We have to exclude everything
+  return if ($self->param('exclude_ehive') and $self->param('exclude_list') and scalar(@$ignores) == $self->param('nb_ehive_tables'));
+
+  # mysqldump command
+  my $output = "";
+  if ($self->param('output_file')) {
+    if (lc $self->param('output_file') =~ /\.gz$/) {
+      $output = sprintf(' | gzip > %s', $self->param('output_file'));
+    } else {
+      $output = sprintf('> %s', $self->param('output_file'));
+    }
+  } else {
+    $output = join(' ', '|', @{ $self->param('real_output_db')->to_cmd(undef, undef, undef, undef, 1) } );
+  };
+
+  # Must be joined because of the redirection / the pipe
+  my $cmd = join(' ', 
+    @{ $self->mysqldump($src_dbc, 1) },
+    '--skip-lock-tables',
+    @$tables,
+    (map {sprintf('--ignore-table=%s.%s', $src_dbc->dbname, $_)} @$ignores),
+    $output
+  );
+  print "$cmd\n" if $self->debug;
+
+  # Check whether the current database has been restored from a snapshot.
+  # If it is the case, we shouldn't re-dump and overwrite the file.
+  # We also check here the value of the "skip_dump" parameter
+  my $completion_signature = sprintf('dump_%d_restored', $self->input_job->dbID < 0 ? 0 : $self->input_job->dbID);
+  return if $self->param('skip_dump') or $self->param($completion_signature);
+
+  # OK, we can dump
+  if(my $return_value = system($cmd)) {
+      die "system( $cmd ) failed: $return_value";
+  }
+
+  # We add the signature to the dump, so that the job won't rerun on a
+  # restored database
+  my $extra_sql = qq{echo "INSERT INTO pipeline_wide_parameters VALUES ('$completion_signature', 1);\n" $output};
+  # We're very lucky that gzipped streams can be concatenated and the
+  # output is still valid !
+  $extra_sql =~ s/>/>>/;
+  if(my $return_value = system($extra_sql)) {
+      die "system( $extra_sql ) failed: $return_value";
+  }
+}
+
+our $pass_internal_counter = 0;
+sub mysqldump {
+  my ($self, $dbc, $hide_password_in_env) = @_;
+
+  my $hidden_password;
+    if ($dbc->password) {
+      if ($hide_password_in_env) {
+        my $pass_variable = "EHIVE_TMP_PASSWORD_${pass_internal_counter}";
+        $pass_internal_counter++;
+        $ENV{$pass_variable} = $dbc->password;
+        $hidden_password = '$'.$pass_variable;
+      } else {
+        $hidden_password = $dbc->password;
+      }
+  }
+
+  my @cmd;
+  push @cmd, 'mysqldump';
+  push @cmd, '-h'.$dbc->host       if $dbc->host;
+  push @cmd, '-P'.$dbc->port       if $dbc->port;
+  push @cmd, '-u'.$dbc->username   if $dbc->username;
+  push @cmd, '-p'.$hidden_password if $dbc->password;
+  push @cmd, $dbc->dbname          if $dbc->dbname;
+
+  return \@cmd;
+}
+
 1;
