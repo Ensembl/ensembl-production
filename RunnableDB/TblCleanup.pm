e340683aa16c9d515a4147230e38500ee6190ca7
Thomas Maurel <maurel@ebi.ac.uk>
2015-12-11 16:11:14 +0000

    Moved projection clean up functions from base to new tblCleanup module

--- RunnableDB/TblCleanup.pm
+++ RunnableDB/TblCleanup.pm
@@ -36,24 +36,13 @@ use Bio::EnsEMBL::Utils::SqlHelper;
 sub fetch_input {
     my ($self) 	= @_;
 
-    my $flag_GeneNames = $self->param('flag_GeneNames');
-    my $flag_GeneDescr = $self->param('flag_GeneDescr');
-
-    my $sql_update  = 'UPDATE gene SET display_xref_id = NULL 
-		      WHERE display_xref_id IN 
-                      (SELECT xref_id FROM xref WHERE info_text rlike "projected from")';
-
-    my $sql_update2 = 'UPDATE gene SET description = ""
-		       WHERE description like "%Source%"';
-			
-    my $sql_delete  = 'DELETE FROM xref WHERE info_text rlike "projected from"';
-    
+    my $flag_delete_gene_names = $self->param('flag_delete_gene_names');
+    my $flag_delete_gene_descriptions = $self->param('flag_delete_gene_descriptions');
+    my $flag_delete_go_terms = $self->param('flag_delete_go_terms');
  
-    $self->param('flag_GeneNames', $flag_GeneNames);
-    $self->param('flag_GeneDescr', $flag_GeneDescr);
-    $self->param('sql_update',  $sql_update);
-    $self->param('sql_update2', $sql_update2);
-    $self->param('sql_delete',  $sql_delete);
+    $self->param('flag_delete_gene_names', $flag_delete_gene_names);
+    $self->param('flag_delete_gene_descriptions', $flag_delete_gene_descriptions);
+    $self->param('flag_delete_go_terms', $flag_delete_go_terms);
 
 return 0;
 }
@@ -71,23 +60,81 @@ sub run {
 
     my $helper = Bio::EnsEMBL::Utils::SqlHelper->new( -DB_CONNECTION => $self->core_dbc() );
 
-    my $flag_GeneNames = $self->param('flag_GeneNames');
-    my $flag_GeneDescr = $self->param('flag_GeneDescr');
+    my $flag_delete_gene_names = $self->param('flag_delete_gene_names');
+    my $flag_delete_gene_descriptions = $self->param('flag_delete_gene_descriptions');
+    my $flag_delete_go_terms = $self->param('flag_delete_go_terms');
 
-    my $sql_update  = $self->param_required('sql_update');
-    my $sql_update2 = $self->param_required('sql_update2');
-    my $sql_delete  = $self->param_required('sql_delete');
-
-    $helper->execute_update(-SQL => $sql_update2)if($flag_GeneDescr==1);
-    $helper->execute_update(-SQL => $sql_update) if($flag_GeneNames==1);
-    $helper->execute_update(-SQL => $sql_delete) if($flag_GeneNames==1);
+    delete_gene_names($helper) if($flag_delete_gene_names==1);
+    delete_gene_desk($helper) if($flag_delete_gene_descriptions==1);
+    delete_go_terms($helper) if($flag_delete_go_terms==1);
     
     $self->core_dbc()->disconnect_if_idle();
     
 return 0;
 }
 
+=head2 delete_go_terms
 
+=cut
+sub delete_go_terms {
+    my ($self,$helper)       = @_;
+
+    print STDERR "Delete existing projected GO terms in core\n";
+
+    my $sql_del_terms = "DELETE x, ox, gx FROM xref x, external_db e, object_xref ox, ontology_xref gx WHERE x.xref_id=ox.xref_id AND x.external_db_id=e.external_db_id AND ox.object_xref_id=gx.object_xref_id AND e.db_name='GO' AND x.info_type='PROJECTION'";
+
+    $helper->execute_update(-SQL => $sql_del_terms);
+
+    # note don't need to delete synonyms as GO terms don't have any
+    # Also no effect on descriptions or status
+
+return 0;
+}
+
+=head2 delete_gene_names
+
+=cut
+sub delete_gene_names {
+    my ($self,$helper)       = @_;
+
+    print STDERR "Setting projected transcript statuses to NOVEL\n";
+
+    my $sql_del_terms = "UPDATE gene g, xref x, transcript t SET t.display_xref_id = NULL, t.description = NULL, t.status='NOVEL' WHERE g.display_xref_id=x.xref_id AND x.info_type='PROJECTION' AND g.gene_id = t.gene_id";
+    $helper->execute_update(-SQL => $sql_del_terms);
+
+    print STDERR "Setting gene display_xrefs that were projected to NULL and status to NOVEL\n\n";
+
+    $sql_del_terms = "UPDATE gene g, xref x SET g.display_xref_id = NULL, g.status='NOVEL' WHERE g.display_xref_id=x.xref_id AND x.info_type='PROJECTION'";
+
+    $helper->execute_update(-SQL => $sql_del_terms);
+
+    print STDERR "Deleting projected xrefs, object_xrefs and synonyms\n";
+
+    $sql_del_terms = "DELETE es FROM xref x, external_synonym es WHERE x.xref_id=es.xref_id AND x.info_type='PROJECTION'";
+
+    $helper->execute_update(-SQL => $sql_del_terms);
+
+    $sql_del_terms="DELETE x, ox FROM xref x, object_xref ox, external_db e WHERE x.xref_id=ox.xref_id AND x.external_db_id=e.external_db_id AND x.info_type='PROJECTION' AND e.db_name!='GO'";
+
+    $helper->execute_update(-SQL => $sql_del_terms);
+
+return 0;
+}
+
+=head2 delete_gene_desc
+
+=cut
+sub delete_gene_desc {
+    my ($self,$helper)       = @_;
+
+    print STDERR "Setting descriptions that were projected to NULL \n";
+
+    my $sql_del_terms = "UPDATE gene g, xref x SET g.description=NULL WHERE g.display_xref_id=x.xref_id AND x.info_type='PROJECTION'";
+
+    $helper->execute_update(-SQL => $sql_del_terms);
+
+return 0;
+}
 
 1;
 
