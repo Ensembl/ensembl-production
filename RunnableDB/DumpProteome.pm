ce7e19824943b3d1fc99065bf226bc0ce7192cf1
thomasmaurel <maurel@ebi.ac.uk>
2016-04-04 16:18:27 +0100

    Added support for biotype grouping. Now the pipeline will get all the biotypes from the ensembl_production database

--- RunnableDB/DumpProteome.pm
+++ RunnableDB/DumpProteome.pm
@@ -82,6 +82,22 @@ sub run {
   my $line_width        = $self->param('line_width');
   my $allow_stop_codons = $self->param('allow_stop_codons');
   
+  # Use the ensembl_production database
+  # to retrieve the biotypes associated to the coding group
+  my $biotypes;
+  my $biotype_groups = ['coding'];
+  # work out what biotypes correspond to $transcript_type
+  my $pdba = $self->get_DBAdaptor('production');
+
+  unless(defined $pdba){
+    my %prod_db = %{$self->param_required('prod_db')};
+    $pdba       = Bio::EnsEMBL::Production::DBSQL::DBAdaptor->new(%prod_db);
+  }
+
+  my $biotype_manager = $pdba->get_biotype_manager();
+
+  map { push @{$biotypes}, @{ $biotype_manager->group_members($_)} } @{$biotype_groups};
+
   open(my $fh, '>', $proteome_file) or $self->throw("Cannot open file $proteome_file: $!");
   my $serializer = Bio::EnsEMBL::Utils::IO::FASTASerializer->new(
     $fh,
@@ -92,7 +108,7 @@ sub run {
   
   my $dba = $self->core_dba();
   my $tra = $dba->get_adaptor('Transcript');
-  my $transcripts = $tra->fetch_all_by_biotype('protein_coding');
+  my $transcripts = $tra->fetch_all_by_biotype($biotypes);
   
   foreach my $transcript (sort { $a->stable_id cmp $b->stable_id } @{$transcripts}) {
     my $seq_obj = $transcript->translate();
