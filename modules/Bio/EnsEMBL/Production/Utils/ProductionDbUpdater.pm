4f2969e2c66d7b524d9f6e63a3860b1a435e00f0
Dan Staines <dstaines@ebi.ac.uk>
2016-06-08 13:31:19 +0100

    just process analyses that we have

--- modules/Bio/EnsEMBL/Production/Utils/ProductionDbUpdater.pm
+++ modules/Bio/EnsEMBL/Production/Utils/ProductionDbUpdater.pm
@@ -21,10 +21,15 @@ limitations under the License.
 
 =head1 SYNOPSIS
 
+  my $updater = Bio::EnsEMBL::Production::Utils::ProductionDbUpdater->new(
+                      -PRODUCTION_DBA => $prod_dba);
+                      
+  $updater->update_controlled_tables($dbc);
 
 =head1 DESCRIPTION
 
   Utility for updating a database from the Ensembl production database
+  Used by update_controlled_tables.pl
 
 =cut
 
@@ -56,18 +61,7 @@ sub new {
   return $self;
 }
 
-sub backup_table {
-  my ( $self, $dbc, $table ) = @_;
-  if ( $self->{backup} == 1 ) {
-    $logger->info("Backing up $table");
-    $dbc->sql_helper->execute_update(
-                                  -SQL => "DROP TABLE IF EXISTS ${table}_bak" );
-    $dbc->sql_helper->execute_update(
-                  -SQL => "CREATE TABLE ${table}_bak AS SELECT * FROM $table" );
-  }
-  return;
-}
-
+# definition of supported tables
 my $tables = {
   external_db => {
     label => 'db_name',
@@ -105,51 +99,6 @@ my $tables = {
       row_id   => 'unmapped_reason_id' }
 
 };
-
-sub get_production_table {
-  my ( $self, $table ) = @_;
-  if ( !defined $self->{$table} ) {
-    $logger->info("Retrieving production $table");
-    $self->{$table} =
-      $self->get_data_from_table( $self->{prod_dba}->dbc(),
-                                  get_select($table),
-                                  $tables->{$table}{row_id} );
-  }
-  return $self->{$table};
-}
-
-sub get_data_from_table {
-  my ( $self, $dbc, $sql, $row_id ) = @_;
-  return {
-    map { $_->{$row_id} => $_ } @{
-      $dbc->sql_helper()->execute( -USE_HASHREFS => 1, -SQL => $sql );
-    } };
-}
-
-sub get_select {
-  my ($table) = @_;
-  return 'SELECT ' . join( ',', @{ $tables->{$table}{cols} } ) . ' FROM ' .
-    $table;
-}
-
-sub get_insert {
-  my ($table) = @_;
-  return 'INSERT INTO ' . $table . ' (' .
-    join( ', ', @{ $tables->{$table}{all_cols} } ) . ') VALUES (' .
-    join( ', ', ( map { "?" } @{ $tables->{$table}{all_cols} } ) ) . ')';
-}
-
-sub get_update {
-  my ($table) = @_;
-  return 'UPDATE ' . $table . ' SET ' .
-    join( ', ', ( map { "$_ = ?" } @{ $tables->{$table}{cols} } ) ) .
-    ' WHERE ' . $tables->{$table}{row_id} . ' = ?';
-}
-
-sub get_delete {
-  my ($table) = @_;
-  return ' DELETE FROM ' . $table . ' WHERE ' . $tables->{row_id} . ' = ?';
-}
 
 sub update_controlled_tables {
   my ( $self, $dbc ) = @_;
@@ -220,13 +169,12 @@ sub update_controlled_table {
                -PARAMS => [
                  ( map { $mdata->{$row_id}{$_} } @{ $tables->{$table}{cols} } ),
                  $row_id ] );
-            $logger->info(
-                    "Updated data for id $row_id (" . $mdata->{$row_id}{$tables->{$table}{label}} .
-                      ")" );
+            $logger->info( "Updated data for id $row_id (" .
+                          $mdata->{$row_id}{ $tables->{$table}{label} } . ")" );
           }
         }
       }
-    } ## end if ( $self->{update} ==...)
+    }
 
     if ( $self->{delete} == 1 ) {
       $logger->info("Deleting old rows");
@@ -234,8 +182,8 @@ sub update_controlled_table {
         if ( !exists $mdata->{$row_id} ) {
           $helper->execute_update( -SQL    => get_delete($table),
                                    -PARAMS => [$row_id] );
-          $logger->info(
-              "Deleted data for id $row_id (" . $data->{$row_id}{$tables->{$table}{label}} . ")" );
+          $logger->info( "Deleted data for id $row_id (" .
+                         $data->{$row_id}{ $tables->{$table}{label} } . ")" );
         }
       }
     }
@@ -248,6 +196,128 @@ sub update_controlled_table {
     throw "Do not know how to update table $table";
   }
 } ## end sub update_controlled_table
+
+sub update_analysis_description {
+  my ( $self, $dbc, $species, $type, $logic_names ) = @_;
+
+  throw "species and type are both required"
+    if ( ( $species && !$type ) || ( !$species && $type ) );
+
+  my $dbname = $dbc->dbname();
+
+  ( $species, $type ) = ( $dbname =~ m/^([^_]+_[^_]+)_([^_]+)/ );
+
+  throw "Could not determine species and type from $dbname"
+    unless ( $species && $type );
+
+  if ( !defined $logic_names || scalar(@$logic_names)==0 ) {
+    $logic_names =
+      $dbc->sql_helper()
+      ->execute_simple( -SQL =>
+'select logic_name from analysis join analysis_description using (analysis_id)'
+      );
+  }
+  $logic_names = { map { $_ => 1 } @$logic_names };
+
+  $self->backup_table( $dbc, "analysis_description" );
+
+  $logger->info("Retrieving analyses for $species/$type");
+  my $analysis_types = $self->get_data_from_table(
+    $self->{prod_dba}->dbc(), q/
+    SELECT ad.logic_name as logic_name, ad.description as description, 
+    ad.display_label as display_label, wd.data as web_data, aw.displayable as displayable
+    FROM analysis_description ad, species s, analysis_web_data aw 
+    LEFT OUTER JOIN web_data wd ON aw.web_data_id = wd.web_data_id 
+    WHERE ad.analysis_description_id = aw.analysis_description_id 
+    AND aw.species_id = s.species_id 
+    AND s.db_name = ? 
+    AND aw.db_type =?/, 'logic_name' );
+
+  $logger->info("Retrieving generic analyses");
+  while ( my ( $k, $v ) = each %{ $self->get_generic_analyses() } ) {
+    if ( !exists $analysis_types->{$k} ) {
+      $analysis_types->{$k} = $v;
+    }
+  }
+
+  $logger->info("Updating analyses");
+  while ( my ( $logic_name, $analysis ) = each %{$analysis_types} ) {
+    if ( scalar( keys %$logic_names ) == 0 ||
+         exists $logic_names->{$logic_name} )
+    {
+      $logger->debug("Updating $logic_name");
+      if ( defined $analysis->{web_data} ) {
+        $analysis->{web_data} = eval( $analysis->{web_data} );
+      }
+      $dbc->sql_helper->execute_update(
+        -SQL =>
+          q/UPDATE analysis_description ad JOIN analysis a USING (analysis_id) 
+       SET ad.description=?,ad.display_label=?,ad.web_data=?,ad.displayable=?
+       WHERE logic_name=?/,
+        -PARAMS => [ $analysis->{description}, $analysis->{display_label},
+                     $analysis->{web_data},    $analysis->{displayable} ] );
+    }
+  }
+  $logger->info( "Completed updating analysis_description for " . $dbname );
+  return;
+} ## end sub update_analysis_description
+
+sub backup_table {
+  my ( $self, $dbc, $table ) = @_;
+  if ( $self->{backup} == 1 ) {
+    $logger->info("Backing up $table");
+    $dbc->sql_helper->execute_update(
+                                  -SQL => "DROP TABLE IF EXISTS ${table}_bak" );
+    $dbc->sql_helper->execute_update(
+                  -SQL => "CREATE TABLE ${table}_bak AS SELECT * FROM $table" );
+  }
+  return;
+}
+
+sub get_production_table {
+  my ( $self, $table ) = @_;
+  if ( !defined $self->{$table} ) {
+    $logger->info("Retrieving production $table");
+    $self->{$table} =
+      $self->get_data_from_table( $self->{prod_dba}->dbc(),
+                                  get_select($table),
+                                  $tables->{$table}{row_id} );
+  }
+  return $self->{$table};
+}
+
+sub get_data_from_table {
+  my ( $self, $dbc, $sql, $row_id ) = @_;
+  return {
+    map { $_->{$row_id} => $_ } @{
+      $dbc->sql_helper()->execute( -USE_HASHREFS => 1, -SQL => $sql );
+    } };
+}
+
+sub get_select {
+  my ($table) = @_;
+  return 'SELECT ' . join( ',', @{ $tables->{$table}{cols} } ) . ' FROM ' .
+    $table;
+}
+
+sub get_insert {
+  my ($table) = @_;
+  return 'INSERT INTO ' . $table . ' (' .
+    join( ', ', @{ $tables->{$table}{all_cols} } ) . ') VALUES (' .
+    join( ', ', ( map { "?" } @{ $tables->{$table}{all_cols} } ) ) . ')';
+}
+
+sub get_update {
+  my ($table) = @_;
+  return 'UPDATE ' . $table . ' SET ' .
+    join( ', ', ( map { "$_ = ?" } @{ $tables->{$table}{cols} } ) ) .
+    ' WHERE ' . $tables->{$table}{row_id} . ' = ?';
+}
+
+sub get_delete {
+  my ($table) = @_;
+  return ' DELETE FROM ' . $table . ' WHERE ' . $tables->{row_id} . ' = ?';
+}
 
 sub get_prod_analysis_types {
   my ($self) = @_;
@@ -279,65 +349,6 @@ sub get_generic_analyses {
   }
   return $self->{analyses};
 }
-
-sub update_analysis_description {
-  my ( $self, $dbc, $species, $type, $logic_names ) = @_;
-
-  if(defined $logic_names) {
-    $logic_names = {map { $_ => 1 } @$logic_names};
-  } else {
-    $logic_names = {};
-  }
-
-  throw "species and type are both required" if ( ($species && !$type) || (!$species && $type) );
-
-  my $dbname = $dbc->dbname();  
-
-  ( $species, $type ) = ( $dbname =~ m/^([^_]+_[^_]+)_([^_]+)/ );
-
-  throw "Could not determine species and type from $dbname"
-    unless ( $species && $type );
-
-  $self->backup_table( $dbc, "analysis_description" );
-
-  $logger->info("Retrieving analyses for $species/$type");
-  my $analysis_types = $self->get_data_from_table(
-    $self->{prod_dba}->dbc(), q/
-    SELECT ad.logic_name as logic_name, ad.description as description, 
-    ad.display_label as display_label, wd.data as web_data, aw.displayable as displayable
-    FROM analysis_description ad, species s, analysis_web_data aw 
-    LEFT OUTER JOIN web_data wd ON aw.web_data_id = wd.web_data_id 
-    WHERE ad.analysis_description_id = aw.analysis_description_id 
-    AND aw.species_id = s.species_id 
-    AND s.db_name = ? 
-    AND aw.db_type =?/, 'logic_name' );
-
-  $logger->info("Retrieving generic analyses");
-  while ( my ( $k, $v ) = each %{ $self->get_generic_analyses() } ) {
-    if ( !exists $analysis_types->{$k} ) {
-      $analysis_types->{$k} = $v;
-    }
-  }
-
-  $logger->info("Updating analyses");
-  while ( my ( $logic_name, $analysis ) = each %{$analysis_types} ) {
-     if (scalar(keys %$logic_names) == 0 || exists $logic_names->{$logic_name}) {
-    $logger->debug("Updating $logic_name");
-    if ( defined $analysis->{web_data} ) {
-      $analysis->{web_data} = eval( $analysis->{web_data} );
-    }
-    $dbc->sql_helper->execute_update(
-      -SQL =>
-        q/UPDATE analysis_description ad JOIN analysis a USING (analysis_id) 
-       SET ad.description=?,ad.display_label=?,ad.web_data=?,ad.displayable=?
-       WHERE logic_name=?/,
-      -PARAMS => [ $analysis->{description}, $analysis->{display_label},
-                   $analysis->{web_data},    $analysis->{displayable} ] );
-  }
-  }
-  $logger->info( "Completed updating analysis_description for " . $dbname );
-  return;
-} ## end sub update_analysis_description
 
 1;
 
