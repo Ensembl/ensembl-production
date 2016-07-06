9d1b098c755d04cf94c30ff5f06e2a467757d963
Dan Staines <dstaines@ebi.ac.uk>
2016-07-06 14:05:06 +0100

    EG-specific OBO files

---
+++ scripts/ontology/scripts/retrieve_obo.sh
@@ -0,0 +1,23 @@
+#wget "http://curation.pombase.org/dumps/releases/pombase-chado-v$PB_VERSION-$PB_RELEASE/pombe-embl/mini-ontologies/fypo_extension.obo"
+#wget "http://curation.pombase.org/dumps/releases/pombase-chado-v$PB_VERSION-$PB_RELEASE/pombe-embl/mini-ontologies/quiescence.obo" -O PBQ.obo
+#wget "http://curation.pombase.org/dumps/releases/pombase-chado-v$PB_VERSION-$PB_RELEASE/ontologies/go.obo"
+#wget "http://curation.pombase.org/dumps/releases/pombase-chado-v$PB_VERSION-$PB_RELEASE/ontologies/fypo-simple.obo" -O fypo.obo
+#wget "http://sourceforge.net/p/pombase/code/HEAD/tree/phenotype_ontology/releases/latest/fypo-simple.obo?format=raw" -O FYPO.obo
+#wget "http://curation.pombase.org/dumps/releases/pombase-chado-v$PB_VERSION-$PB_RELEASE/pombe-embl/mini-ontologies/pombe_mini_PR.obo" -O PRO.obo
+#wget "http://curation.pombase.org/dumps/releases/pombase-chado-v$PB_VERSION-$PB_RELEASE/pombe-embl/mini-ontologies/chebi.obo" -O chebi.obo
+wget "http://www.geneontology.org/ontology/obo_format_1_2/gene_ontology.1_2.obo" -O GO.obo
+wget "http://sourceforge.net/p/song/svn/HEAD/tree/trunk/so-xp.obo?format=raw" -O SO.obo
+wget "http://palea.cgrb.oregonstate.edu/viewsvn/Poc/trunk/ontology/OBO_format/plant_ontology.obo?view=co" -O PO.obo
+wget "http://palea.cgrb.oregonstate.edu/viewsvn/Poc/trunk/ontology/collaborators_ontology/gramene/temporal_gramene.obo?view=co" -O GRO.obo
+wget "http://palea.cgrb.oregonstate.edu/viewsvn/Poc/trunk/ontology/collaborators_ontology/gramene/taxonomy/GR_tax-ontology.obo?view=co" -O GR_TAX.obo
+wget "http://obo.cvs.sourceforge.net/viewvc/obo/obo/ontology/phenotype/environment/environment_ontology.obo?view=co" -O EO.obo
+wget "http://palea.cgrb.oregonstate.edu/viewsvn/Poc/trunk/ontology/collaborators_ontology/gramene/traits/trait.obo?view=co" -O TO.obo
+wget "http://svn.code.sf.net/p/efo/code/trunk/src/efoinobo/efo.obo" -O EFO.obo
+wget "http://sourceforge.net/p/pombase/code/HEAD/tree/phenotype_ontology/peco.obo?format=raw" -O PECO.obo
+wget "http://purl.obolibrary.org/obo/cl-basic.obo" -O CL.obo
+wget "https://raw.githubusercontent.com/pato-ontology/pato/master/pato.obo" -O PATO.obo
+wget "http://www.berkeleybop.org/ontologies/obo-all/OGMS/OGMS.obo" -O OGMS.obo
+wget "http://www.brenda-enzymes.info/ontology/tissue/tree/update/update_files/BrendaTissueOBO" -O BTO.obo
+wget "http://www.berkeleybop.org/ontologies/obo-all/bfo/bfo.obo" -O BFO.obo
+wget "https://raw.githubusercontent.com/bio-ontology-research-group/unit-ontology/master/unit.obo" -O UO.obo
+wget "https://raw.githubusercontent.com/evidenceontology/evidenceontology/master/eco.obo" -O ECO.obo
