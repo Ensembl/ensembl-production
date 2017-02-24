36eaa866f8c10cd4244d693696c6d997acd6597e
Dan Staines <dstaines@ebi.ac.uk>
2017-02-24 09:40:35 +0000

    reformatted for clarity

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/DumpOrtholog_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/DumpOrtholog_conf.pm
@@ -1,3 +1,4 @@
+
 =head1 LICENSE
 
 Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
@@ -26,233 +27,289 @@ limitations under the License.
  ckong@ebi.ac.uk 
 
 =cut
+
 package Bio::EnsEMBL::Production::Pipeline::PipeConfig::DumpOrtholog_conf;
 
 use strict;
 use warnings;
 use Bio::EnsEMBL::Hive::Version 2.3;
 use Bio::EnsEMBL::ApiVersion qw/software_version/;
-use base ('Bio::EnsEMBL::Hive::PipeConfig::EnsemblGeneric_conf');  
+use base ('Bio::EnsEMBL::Hive::PipeConfig::EnsemblGeneric_conf');
 
 sub default_options {
-    my ($self) = @_;
+	my ($self) = @_;
 
-    return {
-        # inherit other stuff from the base class
-        %{ $self->SUPER::default_options() },      
+	return {
+		# inherit other stuff from the base class
+		%{ $self->SUPER::default_options() },
 
-        'registry'         => '',   
-        'pipeline_name'    => $self->o('hive_dbname'),       
-        'output_dir'       => '/nfs/ftp/pub/databases/ensembl/projections/',     
+		'registry'         => '',
+		'pipeline_name'    => $self->o('hive_dbname'),
+		'output_dir'       => '/nfs/ftp/pub/databases/ensembl/projections/',
 		'method_link_type' => 'ENSEMBL_ORTHOLOGUES',
 
-     	## Set to '1' for eg! run 
-        #   default => OFF (0)
-  	    'eg' => 1,
+		## Set to '1' for eg! run
+		#   default => OFF (0)
+		'eg' => 1,
 
-        # hive_capacity values for analysis
-	    'getOrthologs_capacity'  => '50',
+		# hive_capacity values for analysis
+		'getOrthologs_capacity' => '50',
 
-        # orthologs cutoff
-        'perc_id'  => '30',
-        'perc_cov' => '66',
+		# orthologs cutoff
+		'perc_id'  => '30',
+		'perc_cov' => '66',
 
-        # 'target' & 'exclude' are mutually exclusive
-        #  only one of those should be defined if used 
-	 	'species_config' => 
-		{ 
-		  # Plants
-          'EPl_1' => {	# compara database to get orthologs from
-                   		#  'plants', 'protists', 'fungi', 'metazoa', 'multi'
-	          		   	'compara'  => 'plants',   
-    	               	# source species to project from
- 					   	'source'   => 'arabidopsis_thaliana', 
-            	       	# target species to project to (DEFAULT: undef)
-						'target'   => undef,
-                   		# target species to exclude in projection
-	                   	'exclude'  => ['caenorhabditis_elegans', 'drosophila_melanogaster', 'homo_sapiens', 'ciona_savignyi'], 
-  					   	'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-            	     },   
+		# 'target' & 'exclude' are mutually exclusive
+		#  only one of those should be defined if used
+		'species_config' => {
+			# Plants
+			'EPl_1' => {    # compara database to get orthologs from
+				            #  'plants', 'protists', 'fungi', 'metazoa', 'multi'
+				'compara' => 'plants',
+				# source species to project from
+				'source' => 'arabidopsis_thaliana',
+				# target species to project to (DEFAULT: undef)
+				'target' => undef,
+				# target species to exclude in projection
+				'exclude' => [ 'caenorhabditis_elegans',
+							   'drosophila_melanogaster',
+							   'homo_sapiens',
+							   'ciona_savignyi' ],
+				'homology_types' =>
+				  [ 'ortholog_one2one', 'apparent_ortholog_one2one' ], },
 
-          'EPl_2' => { 'compara'  => 'plants',   
- 					   'source'   => 'oryza_sativa', 
-					   'target'   => undef,
-                   	   'exclude'  => ['caenorhabditis_elegans', 'drosophila_melanogaster', 'homo_sapiens', 'ciona_savignyi'], 
-  				   	   'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-                 	 },   
+			'EPl_2' => {
+				'compara' => 'plants',
+				'source'  => 'oryza_sativa',
+				'target'  => undef,
+				'exclude' => [ 'caenorhabditis_elegans',
+							   'drosophila_melanogaster',
+							   'homo_sapiens',
+							   'ciona_savignyi' ],
+				'homology_types' =>
+				  [ 'ortholog_one2one', 'apparent_ortholog_one2one' ],
+			},
 
-		  # Protists, dictyostelium_discoideum to all amoebozoa
-          'EPr_1' => { 'compara'  => 'protists',   
- 				   	   'source'   => 'dictyostelium_discoideum', 
-					   'target'   => ['polysphondylium_pallidum_pn500', 'entamoeba_nuttalli_p19', 'entamoeba_invadens_ip1', 'entamoeba_histolytica_ku27', 'entamoeba_histolytica_hm_3_imss', 'entamoeba_histolytica_hm_1_imss_b', 'entamoeba_histolytica_hm_1_imss_a', 'entamoeba_histolytica', 'entamoeba_dispar_saw760', 'dictyostelium_purpureum', 'dictyostelium_fasciculatum', 'acanthamoeba_castellanii_str_neff'],
-                   	   'exclude'  => undef, 
-  				   	   'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-                 	 },   
+			# Protists, dictyostelium_discoideum to all amoebozoa
+			'EPr_1' => { 'compara' => 'protists',
+						 'source'  => 'dictyostelium_discoideum',
+						 'target'  => ['polysphondylium_pallidum_pn500',
+									   'entamoeba_nuttalli_p19',
+									   'entamoeba_invadens_ip1',
+									   'entamoeba_histolytica_ku27',
+									   'entamoeba_histolytica_hm_3_imss',
+									   'entamoeba_histolytica_hm_1_imss_b',
+									   'entamoeba_histolytica_hm_1_imss_a',
+									   'entamoeba_histolytica',
+									   'entamoeba_dispar_saw760',
+									   'dictyostelium_purpureum',
+									   'dictyostelium_fasciculatum',
+									   'acanthamoeba_castellanii_str_neff' ],
+						 'exclude' => undef,
+						 'homology_types' =>
+						   [ 'ortholog_one2one', 'apparent_ortholog_one2one' ],
+			},
 
-          # Fungi
-          'EF_1' => { 'compara'  => 'fungi',   
- 				   	  'source'   => 'schizosaccharomyces_pombe', 
-					  'target'   => undef,
-                   	  'exclude'  => ['saccharomyces_cerevisiae'], 
-  				   	  'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-                 	},   
+			# Fungi
+			'EF_1' => { 'compara' => 'fungi',
+						'source'  => 'schizosaccharomyces_pombe',
+						'target'  => undef,
+						'exclude' => ['saccharomyces_cerevisiae'],
+						'homology_types' =>
+						  [ 'ortholog_one2one', 'apparent_ortholog_one2one' ],
+			},
 
-          'EF_2' => { 'compara'  => 'fungi',   
- 				   	  'source'   => 'saccharomyces_cerevisiae', 
-					  'target'   => undef,
-                   	  'exclude'  => ['schizosaccharomyces_pombe'], 
-  				   	  'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-                 	},   
+			'EF_2' => { 'compara' => 'fungi',
+						'source'  => 'saccharomyces_cerevisiae',
+						'target'  => undef,
+						'exclude' => ['schizosaccharomyces_pombe'],
+						'homology_types' =>
+						  [ 'ortholog_one2one', 'apparent_ortholog_one2one' ],
+			},
 
-          # Metazoa
-          'EM_1' => { 'compara'  => 'metazoa',   
- 				   	  'source'   => 'caenorhabditis_elegans', 
-					  'target'   => ['caenorhabditis_brenneri', 'caenorhabditis_briggsae', 'caenorhabditis_japonica', 'caenorhabditis_remanei', 'pristionchus_pacificus', 'brugia_malayi', 'onchocerca_volvulus', 'strongyloides_ratti', 'loa_loa', 'trichinella_spiralis'],
-                   	  'exclude'  => undef, 
-  				   	  'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-                 	},   
+			# Metazoa
+			'EM_1' => { 'compara' => 'metazoa',
+						'source'  => 'caenorhabditis_elegans',
+						'target'  => ['caenorhabditis_brenneri',
+									  'caenorhabditis_briggsae',
+									  'caenorhabditis_japonica',
+									  'caenorhabditis_remanei',
+									  'pristionchus_pacificus',
+									  'brugia_malayi',
+									  'onchocerca_volvulus',
+									  'strongyloides_ratti',
+									  'loa_loa',
+									  'trichinella_spiralis' ],
+						'exclude' => undef,
+						'homology_types' =>
+						  [ 'ortholog_one2one', 'apparent_ortholog_one2one' ],
+			},
 
-          'EM_2' => { 'compara'  => 'metazoa',   
- 				   	  'source'   => 'drosophila_melanogaster', 
-					  'target'   => ['drosophila_ananassae', 'drosophila_erecta', 'drosophila_grimshawi', 'drosophila_mojavensis','drosophila_persimilis' ,'drosophila_pseudoobscura', 'drosophila_sechellia', 'drosophila_simulans', 'drosophila_virilis', 'drosophila_willistoni', 'drosophila_yakuba'],
-                   	  'exclude'  => undef, 
-  				   	  'homology_types' => ['ortholog_one2one','apparent_ortholog_one2one'],		       
-                 	},   
+			'EM_2' => {
+				   'compara' => 'metazoa',
+				   'source'  => 'drosophila_melanogaster',
+				   'target'  => [
+							'drosophila_ananassae',  'drosophila_erecta',
+							'drosophila_grimshawi',  'drosophila_mojavensis',
+							'drosophila_persimilis', 'drosophila_pseudoobscura',
+							'drosophila_sechellia',  'drosophila_simulans',
+							'drosophila_virilis',    'drosophila_willistoni',
+							'drosophila_yakuba' ],
+				   'exclude' => undef,
+				   'homology_types' =>
+					 [ 'ortholog_one2one', 'apparent_ortholog_one2one' ], },
 
-    	},
+		},
 
-       'pipeline_db' => {  
-		     -host   => $self->o('hive_host'),
-        	 -port   => $self->o('hive_port'),
-        	 -user   => $self->o('hive_user'),
-        	 -pass   => $self->o('hive_password'),
-	         -dbname => $self->o('hive_dbname'),
-        	 -driver => 'mysql',
-      	},
-		
-    };
-}
+		'pipeline_db' => { -host   => $self->o('hive_host'),
+						   -port   => $self->o('hive_port'),
+						   -user   => $self->o('hive_user'),
+						   -pass   => $self->o('hive_password'),
+						   -dbname => $self->o('hive_dbname'),
+						   -driver => 'mysql', },
+
+	};
+} ## end sub default_options
 
 sub pipeline_create_commands {
-    my ($self) = @_;
-    return [
-      # inheriting database and hive tables' creation
-      @{$self->SUPER::pipeline_create_commands},
-      'mkdir -p '.$self->o('output_dir'),
-    ];
+	my ($self) = @_;
+	return [
+		# inheriting database and hive tables' creation
+		@{ $self->SUPER::pipeline_create_commands },
+		'mkdir -p ' . $self->o('output_dir'), ];
 }
 
 # Ensures output parameters gets propagated implicitly
 sub hive_meta_table {
-  my ($self) = @_;
-  
-  return {
-    %{$self->SUPER::hive_meta_table},
-    'hive_use_param_stack'  => 1,
-  };
+	my ($self) = @_;
+
+	return { %{ $self->SUPER::hive_meta_table }, 'hive_use_param_stack' => 1, };
 }
 
 # override the default method, to force an automatic loading of the registry in all workers
 sub beekeeper_extra_cmdline_options {
-  my ($self) = @_;
-  return 
-      ' -reg_conf ' . $self->o('registry'),
-  ;
+	my ($self) = @_;
+	return ' -reg_conf ' . $self->o('registry'),;
 }
 
-# these parameter values are visible to all analyses, 
+# these parameter values are visible to all analyses,
 # can be overridden by parameters{} and input_id{}
-sub pipeline_wide_parameters {  
-    my ($self) = @_;
-    return {
-            %{$self->SUPER::pipeline_wide_parameters},  # here we inherit anything from the base class
-            'perc_id'  => $self->o('perc_id'),
-            'perc_cov' => $self->o('perc_cov'),
-    };
+sub pipeline_wide_parameters {
+	my ($self) = @_;
+	return {
+		%{ $self->SUPER::pipeline_wide_parameters }
+		,    # here we inherit anything from the base class
+		'perc_id'  => $self->o('perc_id'),
+		'perc_cov' => $self->o('perc_cov'), };
 }
-
 
 sub pipeline_analyses {
-    my ($self) = @_;
- 
-    return [
-    {  -logic_name    => 'backbone_fire_DumpOrthologs',
-       -module        => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
-       -input_ids     => [ {} ] , 
-       -flow_into 	  => { '1' => ['SourceFactory'], }
-    },   
- 
-    {  -logic_name    => 'SourceFactory',
-       -module        => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::SourceFactory',
-       -parameters    => { 'species_config'  => $self->o('species_config'), }, 
-       -flow_into     => { '2' => ['MLSSJobFactory'], },          
-       -rc_name       => 'default',
-    },    
- 
-    {  -logic_name    => 'MLSSJobFactory',
-       -module        => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::MLSSJobFactory',
-       -parameters    => { 'method_link_type' => $self->o('method_link_type'), },
-       -flow_into     => { '2' => ['GetOrthologs'], },
-       -rc_name       => 'default',
-    },
-  
-    {  -logic_name    => 'GetOrthologs',
-       -module        => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::DumpFile',
-       -parameters    => {	'eg' 			   => $self->o('eg'),
-       						'output_dir'       => $self->o('output_dir'),
-							'method_link_type' => $self->o('method_link_type'),
-    	 				 },
-       -batch_size    =>  1,
-       -rc_name       => 'default',
-	   -hive_capacity => $self->o('getOrthologs_capacity'), 
-	   -flow_into     => { '-1' => 'GetOrthologs_16GB', }, 
-	 },
-	 
-    {  -logic_name    => 'GetOrthologs_16GB',
-       -module        => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::DumpFile',
-       -parameters    => {	'output_dir'             => $self->o('output_dir'),
-							'method_link_type'       => $self->o('method_link_type'),
-    	 				 },
-       -batch_size    =>  1,
-       -rc_name       => '16Gb_mem',
-	   -hive_capacity => $self->o('getOrthologs_capacity'), 
-	   -flow_into     => { '-1' => 'GetOrthologs_32GB', }, 
-	 },
+	my ($self) = @_;
 
-    {  -logic_name    => 'GetOrthologs_32GB',
-       -module        => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::DumpFile',
-       -parameters    => {	'output_dir'             => $self->o('output_dir'),
-							'method_link_type'       => $self->o('method_link_type'),
-    	 				 },
-       -batch_size    =>  1,
-       -rc_name       => '32Gb_mem',
-	   -hive_capacity => $self->o('getOrthologs_capacity'), 
-	 },
-	 	 
-  ];
-}
+	return [ {
+		   -logic_name => 'backbone_fire_DumpOrthologs',
+		   -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+		   -input_ids  => [ {} ],
+		   -flow_into  => { '1' => ['SourceFactory'], } },
+
+		{  -logic_name => 'SourceFactory',
+		   -module =>
+			 'Bio::EnsEMBL::Production::Pipeline::Ortholog::SourceFactory',
+		   -parameters => { 'species_config' => $self->o('species_config'), },
+		   -flow_into  => { '2'              => ['MLSSJobFactory'], },
+		   -rc_name    => 'default', },
+
+		{  -logic_name => 'MLSSJobFactory',
+		   -module =>
+			 'Bio::EnsEMBL::Production::Pipeline::Ortholog::MLSSJobFactory',
+		   -parameters =>
+			 { 'method_link_type' => $self->o('method_link_type'), },
+		   -flow_into => { '2' => ['GetOrthologs'], },
+		   -rc_name   => 'default', },
+
+		{  -logic_name => 'GetOrthologs',
+		   -module => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::DumpFile',
+		   -parameters => { 'eg'               => $self->o('eg'),
+							'output_dir'       => $self->o('output_dir'),
+							'method_link_type' => $self->o('method_link_type'),
+		   },
+		   -batch_size    => 1,
+		   -rc_name       => 'default',
+		   -hive_capacity => $self->o('getOrthologs_capacity'),
+		   -flow_into     => { '-1' => 'GetOrthologs_16GB', }, },
+
+		{  -logic_name => 'GetOrthologs_16GB',
+		   -module => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::DumpFile',
+		   -parameters => { 'output_dir'       => $self->o('output_dir'),
+							'method_link_type' => $self->o('method_link_type'),
+		   },
+		   -batch_size    => 1,
+		   -rc_name       => '16Gb_mem',
+		   -hive_capacity => $self->o('getOrthologs_capacity'),
+		   -flow_into     => { '-1' => 'GetOrthologs_32GB', }, },
+
+		{  -logic_name => 'GetOrthologs_32GB',
+		   -module => 'Bio::EnsEMBL::Production::Pipeline::Ortholog::DumpFile',
+		   -parameters => { 'output_dir'       => $self->o('output_dir'),
+							'method_link_type' => $self->o('method_link_type'),
+		   },
+		   -batch_size    => 1,
+		   -rc_name       => '32Gb_mem',
+		   -hive_capacity => $self->o('getOrthologs_capacity'), },
+
+	];
+} ## end sub pipeline_analyses
 
 sub resource_classes {
-  my ($self) = @_;
-  return {
-    'default'           => {'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000]"'},
-    'normal'            => {'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000]"'},
-    '2Gb_mem'           => {'LSF' => '-q production-rh7 -M  2000 -R "rusage[mem=2000]"'},
-    '4Gb_mem'           => {'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000]"'},
-    '8Gb_mem'           => {'LSF' => '-q production-rh7 -M  8000 -R "rusage[mem=8000]"'},
-    '12Gb_mem'          => {'LSF' => '-q production-rh7 -M 12000 -R "rusage[mem=12000]"'},
-    '16Gb_mem'          => {'LSF' => '-q production-rh7 -M 16000 -R "rusage[mem=16000]"'},
-    '24Gb_mem'          => {'LSF' => '-q production-rh7 -M 24000 -R "rusage[mem=24000]"'},
-    '32Gb_mem'          => {'LSF' => '-q production-rh7 -M 32000 -R "rusage[mem=32000]"'},
-    '2Gb_mem_4Gb_tmp'   => {'LSF' => '-q production-rh7 -M  2000 -R "rusage[mem=2000,tmp=4000]"'},
-    '4Gb_mem_4Gb_tmp'   => {'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000,tmp=4000]"'},
-    '8Gb_mem_4Gb_tmp'   => {'LSF' => '-q production-rh7 -M  8000 -R "rusage[mem=8000,tmp=4000]"'},
-    '12Gb_mem_4Gb_tmp'  => {'LSF' => '-q production-rh7 -M 12000 -R "rusage[mem=12000,tmp=4000]"'},
-    '16Gb_mem_4Gb_tmp'  => {'LSF' => '-q production-rh7 -M 16000 -R "rusage[mem=16000,tmp=4000]"'},
-    '16Gb_mem_16Gb_tmp' => {'LSF' => '-q production-rh7 -M 16000 -R "rusage[mem=16000,tmp=16000]"'},
-    '24Gb_mem_4Gb_tmp'  => {'LSF' => '-q production-rh7 -M 24000 -R "rusage[mem=24000,tmp=4000]"'},
-    '32Gb_mem_4Gb_tmp'  => {'LSF' => '-q production-rh7 -M 32000 -R "rusage[mem=32000,tmp=4000]"'},
-  }
-}
+	my ($self) = @_;
+	return {
+		'default' =>
+		  { 'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000]"' },
+		'normal' =>
+		  { 'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000]"' },
+		'2Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M  2000 -R "rusage[mem=2000]"' },
+		'4Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000]"' },
+		'8Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M  8000 -R "rusage[mem=8000]"' },
+		'12Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M 12000 -R "rusage[mem=12000]"' },
+		'16Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M 16000 -R "rusage[mem=16000]"' },
+		'24Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M 24000 -R "rusage[mem=24000]"' },
+		'32Gb_mem' =>
+		  { 'LSF' => '-q production-rh7 -M 32000 -R "rusage[mem=32000]"' },
+		'2Gb_mem_4Gb_tmp' => {
+			'LSF' => '-q production-rh7 -M  2000 -R "rusage[mem=2000,tmp=4000]"'
+		},
+		'4Gb_mem_4Gb_tmp' => {
+			'LSF' => '-q production-rh7 -M  4000 -R "rusage[mem=4000,tmp=4000]"'
+		},
+		'8Gb_mem_4Gb_tmp' => {
+			'LSF' => '-q production-rh7 -M  8000 -R "rusage[mem=8000,tmp=4000]"'
+		},
+		'12Gb_mem_4Gb_tmp' => {
+				  'LSF' =>
+					'-q production-rh7 -M 12000 -R "rusage[mem=12000,tmp=4000]"'
+		},
+		'16Gb_mem_4Gb_tmp' => {
+				  'LSF' =>
+					'-q production-rh7 -M 16000 -R "rusage[mem=16000,tmp=4000]"'
+		},
+		'16Gb_mem_16Gb_tmp' => {
+				 'LSF' =>
+				   '-q production-rh7 -M 16000 -R "rusage[mem=16000,tmp=16000]"'
+		},
+		'24Gb_mem_4Gb_tmp' => {
+				  'LSF' =>
+					'-q production-rh7 -M 24000 -R "rusage[mem=24000,tmp=4000]"'
+		},
+		'32Gb_mem_4Gb_tmp' => {
+				  'LSF' =>
+					'-q production-rh7 -M 32000 -R "rusage[mem=32000,tmp=4000]"'
+		}, };
+} ## end sub resource_classes
 
 1;
