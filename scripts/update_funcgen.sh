351f940c94e98e3e1ba3713af361b568a07bdc79
Dan Staines <dstaines@ebi.ac.uk>
2017-03-21 12:14:31 +0000

    helper script to check whether seq_regions have been patched for funcgen

---
+++ scripts/update_funcgen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SRV=$1
+
+$SRV -e "show databases" | grep funcgen | grep -v master_schema | while read db; do
+    echo "Checking $db";
+    schema_build=$(echo $db | sed -e 's/.*_\([0-9]*_[0-9]*\)$/\1/')
+    cnt=$($SRV --column-names=false $db -e "select count(*) from seq_region where schema_build<>\"$schema_build\"")
+    echo "$schema_build = $cnt"
+    if [ "$cnt" == "0" ]; then
+	echo "Updating funcgen database"
+	perl -I modules ./scripts/release/update_DB_for_release.pl $($SRV details script) $($SRV details script_dnadb_) -dbname $db -check_displayable -skip_analyse -skip_xref_cleanup
+    fi
+done
