3f6fd6f6b012e14f9636e415b7e618e50e2a7cfc
Dan Staines <dstaines@ebi.ac.uk>
2017-03-23 18:39:26 +0000

    refactored to be multi-species aware and more efficient

--- modules/Bio/EnsEMBL/Production/Pipeline/InterProScan/CleanupProteinFeatures.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/InterProScan/CleanupProteinFeatures.pm
@@ -29,8 +29,7 @@ package Bio::EnsEMBL::Production::Pipeline::InterProScan::CleanupProteinFeatures
 
 use strict;
 use Data::Dumper;
-use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::Utils::SqlHelper;
+use List::MoreUtils qw/natatime/;
 use base ('Bio::EnsEMBL::Production::Pipeline::InterProScan::StoreTsvBase');
 
 sub fetch_input {
@@ -56,38 +55,53 @@ sub run {
     my ($self) = @_;
 
     my $species             = $self->param_required('species');
-    my $translation_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species,'Core','Translation');
-    my $helper              = Bio::EnsEMBL::Utils::SqlHelper->new( -DB_CONNECTION => $self->core_dbc() );
+    my $translation_adaptor = $self->core_dba()->get_TranslationAdaptor();
+    my $helper              = $translation_adaptor->dbc()->sql_helper();
 
     # Query to retrieve all ncoil features
     #  Since the API $t->get_all_ProteinFeatures('ncoils');
     #  Is not returning the correct number of ncoil features
-    my $sql_ncoil_pf = 'SELECT protein_feature_id, translation_id, hit_name, seq_end from protein_feature where hit_name="Coil"';
+    my $sql_ncoil_pf = q/
+SELECT protein_feature_id, translation_id, hit_name, protein_feature.seq_end from protein_feature 
+JOIN translation using (translation_id)
+JOIN transcript using (transcript_id)
+JOIN seq_region using (seq_region_id)
+JOIN coord_system using (coord_system_id)
+where hit_name="Coil" and species_id=?/;
 
-    # Query to remove protein_features fro ncoil analysis, which ends beyond the length of the translation 
-    my $sql_del_feat = 'DELETE FROM protein_feature WHERE protein_feature_id =?';
+    # Query to remove protein_features fro ncoil analysis, which ends beyond the length of the translation     
+    my $to_del = [];
+    my $translation_lengths = {};
+    $helper->execute_no_return( -SQL => $sql_ncoil_pf, -PARAMS=>[$translation_adaptor->species_id()],
+				-CALLBACK => sub {
+				  my ($pf_id, $t_id, $hit_name, $seq_end) = @{shift @_};
+				  my $length = $translation_lengths->{$t_id};
+				  if(!defined $length) {
+				    my $translation = $translation_adaptor->fetch_by_dbID($t_id);
+				    $length = $translation->length();
+				    $translation_lengths->{$t_id} = $length;
+				  }
+				  if($seq_end > $length){
+				    print "$pf_id lies beyond the end of $t_id\n";
+				    push @$to_del, $pf_id;
+				  }
+				}				
+			      );
     
-    my $iterator     = $helper->execute( -SQL => $sql_ncoil_pf,-ITERATOR => 1);
-
-    while($iterator->has_next()) {
-        my $row      = $iterator->next();
-        my $pf_id    = $row->[0];
-        my $t_id     = $row->[1];
-        my $hit_name = $row->[2];        
-        my $seq_end  = $row->[3]; 
-
-        my $translation = $translation_adaptor->fetch_by_dbID($t_id);
-        next unless defined $translation;
-        my $length      = $translation->length();
-
-        if($seq_end > $length){
-            $helper->execute_update(-SQL => $sql_del_feat, -PARAMS =>[$pf_id]);
-        }
-    }
+    print "Found ".scalar(@$to_del)." bad features\n";
+    my $sql_del_feat = 'DELETE FROM protein_feature WHERE protein_feature_id in (';
+    my $it = natatime 1000, @$to_del;
+    while (my @vals = $it->())
+      {	
+	my $sql = $sql_del_feat . join(',',@vals) . ')';
+	die $sql;
+	$helper->execute_update(-SQL=>$sql);
+      }
+    
 
     $translation_adaptor->dbc->disconnect_if_idle();
 
-return 0;
+return;
 }
 
 
