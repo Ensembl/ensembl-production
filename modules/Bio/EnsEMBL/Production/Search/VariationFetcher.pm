36073c62370de4c8b3bb9f246de92fce30401281
Dan Staines <dstaines@ebi.ac.uk>
2017-05-05 10:30:00 +0100

    moved debug statement after variables have been set

--- modules/Bio/EnsEMBL/Production/Search/VariationFetcher.pm
+++ modules/Bio/EnsEMBL/Production/Search/VariationFetcher.pm
@@ -119,7 +119,6 @@ q/SELECT v.variation_id as id, v.name as name, v.source_id as source_id, v.somat
 
 sub _calculate_min_max {
 	my ( $self, $h, $offset, $length ) = @_;
-	$logger->debug("Calculating $offset/$length");
 	if ( !defined $offset ) {
 		$offset = $h->execute_single_result(
 						   -SQL => q/select min(variation_id) from variation/ );
@@ -128,7 +127,7 @@ sub _calculate_min_max {
 		$length = $h->execute_single_result(
 			 -SQL => q/select max(variation_id) from variation/ ) - $offset + 1;
 	}
-
+	$logger->debug("Calculating $offset/$length");
 	my $max = $offset + $length - 1;
 	$logger->debug("Current ID range $offset -> $max");
 	return ( $offset, $max );
