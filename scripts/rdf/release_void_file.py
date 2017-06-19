9059adc065b0748a23d51dac13201aa14e9559f3
Alessandro Vullo <avullo@ebi.ac.uk>
2017-06-19 12:36:53 +0100

    [ENSCORESW-2332]. Correct formatting.

--- scripts/rdf/release_void_file.py
+++ scripts/rdf/release_void_file.py
@@ -15,11 +15,9 @@ from rdflib import URIRef, BNode, Literal, XSD
 from rdflib.namespace import RDF, FOAF, VOID
 
 from project_ftp import ProjectFTP
-from void_rdf import VOIDRDF
+from void_rdf import VoidRDF
 # from qc_void import VOIDQC
 # from github import push_to_branch_repo
-
-import
 
 def usage():
     print("create_void_file.py\n[options]\n\t-p <project> [ensembl|ensemblgenomes] (default: ensembl)\n\t-r <release> (e.g. 89, default: 'current')\n\t-d <release date> (format: DD-MM-YYYY)\n")
@@ -55,7 +53,7 @@ def main(argv):
     speciesInfo = ProjectFTP(project).parseSpecies()
 
     # create Void RDF graph and dump it to file
-    voidFile = "%_void.ttl" % project
+    voidFile = "%s_void.ttl" % project
     voidRdf = VoidRDF(project, release, releaseDate, speciesInfo)
     voidRdf.write(voidFile)
 
@@ -65,7 +63,7 @@ def main(argv):
 
     # # push to the project specific branch of the RDF platform github repo
     # branch = project
-    # token = '' # TODO: how to get a valid one?
+    # token = '' # TODO: how to get a valid one? Read from conf file e! or eg! one
     # push_to_branch_repo(branch, token)
         
 if __name__ == "__main__":
