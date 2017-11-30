602de344ae880e8dcad2526b19b69427d5972d85
Thomas Maurel <maurel@ebi.ac.uk>
2017-11-30 17:01:36 +0000

    Disconnecting from Hive db before database copy

--- modules/Bio/EnsEMBL/Production/Pipeline/CopyDatabases/CopyDatabaseHive.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/CopyDatabases/CopyDatabaseHive.pm
@@ -74,6 +74,9 @@ if(!Log::Log4perl->initialized()) {
   Log::Log4perl->easy_init($DEBUG);
 }
 
+my $hive_dbc = $self->dbc;
+$hive_dbc->disconnect_if_idle() if defined $hive_dbc;
+
 copy_database($source_db_uri, $target_db_uri, $only_tables, $skip_tables, $update, $drop);
 
 my $runtime =  duration(time() - $start_time);
