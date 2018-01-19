14c90693fc80a2053bdf3e17aa1dfbc312612602
Thomas Maurel <maurel@ebi.ac.uk>
2018-01-19 15:47:35 +0000

    Removed mouse strains from antispecies in ftp dumping as we can now dump them. Renamed mus_spretus_spreteij to match new db and URLs names

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/XrefProjection_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/XrefProjection_conf.pm
@@ -148,7 +148,7 @@ sub default_options {
                                 # target species to project to
                                 'species'     => [], # ['puccinia graminis', 'aspergillus_nidulans']
                                 # target species to exclude
-                                'antispecies' => ['mus_musculus','mus_musculus_129s1svimj', 'mus_musculus_aj', 'mus_musculus_akrj', 'mus_musculus_balbcj', 'mus_musculus_c3hhej', 'mus_musculus_c57bl6nj', 'mus_musculus_casteij', 'mus_musculus_cbaj', 'mus_musculus_dba2j', 'mus_musculus_fvbnj', 'mus_musculus_lpj', 'mus_musculus_nodshiltj', 'mus_musculus_nzohlltj', 'mus_musculus_pwkphj', 'mus_musculus_wsbeij', 'mus_spretus_spreteij'],
+                                'antispecies' => ['mus_musculus','mus_musculus_129s1svimj', 'mus_musculus_aj', 'mus_musculus_akrj', 'mus_musculus_balbcj', 'mus_musculus_c3hhej', 'mus_musculus_c57bl6nj', 'mus_musculus_casteij', 'mus_musculus_cbaj', 'mus_musculus_dba2j', 'mus_musculus_fvbnj', 'mus_musculus_lpj', 'mus_musculus_nodshiltj', 'mus_musculus_nzohlltj', 'mus_musculus_pwkphj', 'mus_musculus_wsbeij', 'mus_spretus'],
                                 # target species division to project to
                                 'division'    => [],
                                 # Taxon name of species to project to
@@ -291,7 +291,7 @@ sub default_options {
                                  # target species to project to
                                  'species'     => [], # ['puccinia graminis', 'aspergillus_nidulans']
                                  # target species to exclude
-                                 'antispecies' => ['mus_musculus','mus_musculus_129s1svimj', 'mus_musculus_aj', 'mus_musculus_akrj', 'mus_musculus_balbcj', 'mus_musculus_c3hhej', 'mus_musculus_c57bl6nj', 'mus_musculus_casteij', 'mus_musculus_cbaj', 'mus_musculus_dba2j', 'mus_musculus_fvbnj', 'mus_musculus_lpj', 'mus_musculus_nodshiltj', 'mus_musculus_nzohlltj', 'mus_musculus_pwkphj', 'mus_musculus_wsbeij', 'mus_spretus_spreteij'],
+                                 'antispecies' => ['mus_musculus','mus_musculus_129s1svimj', 'mus_musculus_aj', 'mus_musculus_akrj', 'mus_musculus_balbcj', 'mus_musculus_c3hhej', 'mus_musculus_c57bl6nj', 'mus_musculus_casteij', 'mus_musculus_cbaj', 'mus_musculus_dba2j', 'mus_musculus_fvbnj', 'mus_musculus_lpj', 'mus_musculus_nodshiltj', 'mus_musculus_nzohlltj', 'mus_musculus_pwkphj', 'mus_musculus_wsbeij', 'mus_spretus'],
                                  # target species division to project to
                                  'division'    => [],
                                  # Taxon name of species to project to
--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/XrefProjection_mouse_strains_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/XrefProjection_mouse_strains_conf.pm
@@ -89,7 +89,7 @@ sub default_options {
                                 # target species to project to
                                 'species'     => [], # ['puccinia graminis', 'aspergillus_nidulans']
                                 # target species to exclude
-                                'antispecies' => ['mus_musculus','mus_spretus_spreteij','mus_pahari','mus_caroli'],
+                                'antispecies' => ['mus_musculus','mus_spretus','mus_pahari','mus_caroli'],
                                 # target species division to project to
                                 'division'    => [],
                                 # project all the xrefs instead of display xref only. This is mainly used for the mouse strains at the moment.
@@ -119,7 +119,7 @@ sub default_options {
                                  # source species to project from
                                  'source'      => 'mus_musculus', # 'schizosaccharomyces_pombe'
                                  # target species to project to
-                                 'species'     => ['mus_musculus','mus_spretus_spreteij','mus_pahari','mus_caroli'], # ['puccinia graminis', 'aspergillus_nidulans']
+                                 'species'     => ['mus_musculus','mus_spretus','mus_pahari','mus_caroli'], # ['puccinia graminis', 'aspergillus_nidulans']
                                  # target species to exclude
                                  'antispecies' => [],
                                  # target species division to project to
