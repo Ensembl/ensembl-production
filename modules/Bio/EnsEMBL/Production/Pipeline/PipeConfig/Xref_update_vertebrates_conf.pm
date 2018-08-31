71ef98eae3a3035d4e754107b37ce5495dccf070
Thomas Maurel <maurel@ebi.ac.uk>
2018-08-31 15:29:00 +0100

    We should not run xrefs on mus_musculus_casteij, it should be in the antispecies list with the other mouse strains

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/Xref_update_vertebrates_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/Xref_update_vertebrates_conf.pm
@@ -30,7 +30,7 @@ sub default_options {
     return {
            %{ $self->SUPER::default_options() },
            'division'    => ['EnsemblVertebrates'],
-           'antispecies' => [qw/mus_musculus_129s1svimj mus_musculus_aj mus_musculus_akrj mus_musculus_balbcj mus_musculus_c3hhej mus_musculus_c57bl6nj mus_musculus_cbaj mus_musculus_dba2j mus_musculus_fvbnj mus_musculus_lpj mus_musculus_nodshiltj mus_musculus_nzohlltj mus_musculus_pwkphj mus_musculus_wsbeij drosophila_melanogaster caenorhabditis_elegans saccharomyces_cerevisiae/],
+           'antispecies' => [qw/mus_musculus_129s1svimj mus_musculus_aj mus_musculus_akrj mus_musculus_balbcj mus_musculus_c3hhej mus_musculus_c57bl6nj mus_musculus_casteij mus_musculus_cbaj mus_musculus_dba2j mus_musculus_fvbnj mus_musculus_lpj mus_musculus_nodshiltj mus_musculus_nzohlltj mus_musculus_pwkphj mus_musculus_wsbeij drosophila_melanogaster caenorhabditis_elegans saccharomyces_cerevisiae/],
     };
 }
 
