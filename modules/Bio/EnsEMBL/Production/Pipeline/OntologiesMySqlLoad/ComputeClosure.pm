5f110a583c1681681f77be184c1e978459d4e326
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2018-09-12 14:39:04 +0100

    OLS Ontology loading pipeline

---
+++ modules/Bio/EnsEMBL/Production/Pipeline/OntologiesMySqlLoad/ComputeClosure.pm
@@ -0,0 +1,191 @@
+=head1 LICENSE
+
+Copyright [2009-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+
+package Bio::EnsEMBL::Production::Pipeline::OntologiesMySqlLoad::ComputeClosure;
+use strict;
+use warnings FATAL => 'all';
+use base ('Bio::EnsEMBL::Production::Pipeline::Common::Base');
+use Bio::EnsEMBL::Hive::DBSQL::DBConnection;
+use Cwd ('abs_path', 'getcwd');
+
+
+sub fetch_input {
+}
+
+sub run {
+    my $self = shift @_;
+
+}
+
+sub write_output {
+    my $self = shift @_;
+
+    my $default_relations = [ 'is_a', 'part_of' ];
+    my $config;
+    my $test_eval = eval {require Config::Simple};
+
+    if (!$test_eval) {
+        # The user does not have the 'Config::Simple' module.
+        print(STDERR "No Config::Simple module found, "
+            . "continuing without the ini-file.\n");
+        # If the configuration file *is* an ini-file, we can expect a
+        # load of compilation errors from the next eval...
+    }
+    else {
+        # The user has the 'Config::Simple' module installed.  See
+        # if this is an ini-file or not...
+        my $config_file = $self->param('base_dir') . "/ensembl-production/modules/Bio/EnsEMBL/Production/Pipeline/OntologiesMySqlLoad/closure_config.ini";
+        print "\nIni file path $config_file\n";
+        $config = new Config::Simple($config_file)->vars();
+    }
+
+    my $dbh = Bio::EnsEMBL::Hive::DBSQL::DBConnection->new(-url => $self->param_required('db_url'))->db_handle();
+
+    print "Clearing closure table\n";
+    $dbh->do('TRUNCATE TABLE closure');
+    $dbh->do('ALTER TABLE closure DISABLE KEYS');
+
+    print "Importing intra-ontology parent-child relations\n";
+    $dbh->do(q/
+            INSERT INTO  closure (child_term_id, parent_term_id, distance, subparent_term_id, ontology_id)
+            SELECT  term_id, term_id, 0, NULL, ontology_id
+            FROM  term
+            WHERE  is_obsolete = 0
+        /);
+
+    print "Importing inter-ontology parent-child relations\n";
+    my $rels_join_xaspect = join ',', map {"'$_'"} @$default_relations;
+    $dbh->do(qq/
+            INSERT IGNORE INTO  closure (child_term_id, parent_term_id, distance, subparent_term_id, ontology_id)
+            SELECT DISTINCT r.child_term_id, r.parent_term_id, 0, NULL, r.ontology_id
+            FROM term c
+               JOIN ontology o ON (c.ontology_id=o.ontology_id)
+               JOIN relation r ON (c.term_id=r.child_term_id)
+               JOIN relation_type rt ON (r.relation_type_id=rt.relation_type_id)
+               JOIN term p ON (r.parent_term_id=p.term_id)
+            WHERE c.ontology_id!=p.ontology_id
+            AND rt.name IN ($rels_join_xaspect)
+            AND c.is_obsolete=0
+            AND p.is_obsolete=0
+        /);
+
+    # hash using ontology_name-namespace with list of possible relations
+    print "Importing defined relations\n";
+    my $relations = {};
+    my $sth = $dbh->prepare(q/
+                    SELECT  distinct o.name, o.namespace
+                    FROM  ontology o
+                    JOIN  relation r using (ontology_id)/);
+
+    $sth->execute();
+    my @row;
+    while (@row = $sth->fetchrow_array) {
+        $relations->{ $row[0] }->{ $row[1] } = 1;
+    }
+    $sth->finish();
+
+    for my $ontology (keys %{$relations}) {
+        for my $namespace (keys %{$relations->{$ontology}}) {
+            my $rels = $config->{ $ontology . '.' . $namespace };
+            if (!defined $rels) {
+                $rels = $default_relations;
+            }
+            if (scalar(@$rels) > 0) {
+                my $rels_join = join ',', map {"'$_'"} @$rels;
+                print "Importing $ontology.$namespace $rels_join relations\n";
+                $dbh->do(qq/
+                    INSERT IGNORE INTO  closure (child_term_id, parent_term_id, distance, subparent_term_id, ontology_id)
+                    SELECT DISTINCT r.child_term_id, r.parent_term_id, 1, r.child_term_id, r.ontology_id
+                    FROM term c
+                        JOIN ontology o ON (c.ontology_id=o.ontology_id)
+                        JOIN relation r ON (c.term_id=r.child_term_id)
+                        JOIN relation_type rt ON (r.relation_type_id=rt.relation_type_id)
+                        JOIN term p ON (r.parent_term_id=p.term_id)
+                    WHERE c.ontology_id=p.ontology_id
+                    AND rt.name IN ($rels_join)
+                    AND c.is_obsolete=0
+                    AND p.is_obsolete=0
+                    AND  o.name='$ontology' AND o.namespace='$namespace'
+                /);
+            }
+        }
+    }
+
+    print "Computing closures\n";
+
+    my $select_sth = $dbh->prepare(q/
+                               SELECT DISTINCT
+                                       child.child_term_id,
+                                       parent.parent_term_id,
+                                       child.distance + 1,
+                                       parent.child_term_id,
+                                       child.ontology_id
+                                 FROM  closure child
+                                 JOIN  closure parent
+                                   ON  (parent.child_term_id = child.parent_term_id)
+                                 JOIN  ontology co
+                                   ON  (child.ontology_id=co.ontology_id)
+                                 JOIN  ontology po
+                                   ON  (parent.ontology_id=po.ontology_id)
+                                WHERE  child.distance  = ?
+                                  AND  parent.distance = 1
+                                  AND  co.name = po.name
+                    /);
+
+    my $insert_sth = $dbh->prepare(q/
+                            REPLACE INTO closure (child_term_id, parent_term_id, distance, subparent_term_id, ontology_id)
+                            VALUES  (?, ?, ?, ?, ?)
+                            /);
+
+    my ($oldsize) = $dbh->selectrow_array('SELECT COUNT(1) FROM closure');
+    my $newsize;
+    my $distance = 0;
+
+    local $SIG{ALRM} = sub {
+        printf("Distance = %d, Size = %d\n", $distance, $newsize);
+        alarm(10);
+    };
+    alarm(10);
+
+    while (!defined($newsize) || $newsize > $oldsize) {
+        $oldsize = $newsize || $oldsize;
+        $newsize = $oldsize;
+
+        $dbh->do('LOCK TABLES closure AS child READ, closure AS parent READ, ontology as co READ, ontology as po READ');
+
+        $select_sth->execute(++$distance);
+
+        $dbh->do('LOCK TABLE closure WRITE');
+        while (my @data = $select_sth->fetchrow_array()) {
+            $insert_sth->execute(@data);
+            $newsize++;
+        }
+        $dbh->do('UNLOCK TABLES');
+    }
+    alarm(0);
+
+    print "Computing closures complete - optimising tables\n";
+
+    $dbh->do('ALTER TABLE closure ENABLE KEYS');
+    $dbh->do('OPTIMIZE TABLE closure');
+
+    $dbh->disconnect();
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OntologyMySqlLoad_conf.pm
@@ -0,0 +1,172 @@
+=pod
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Hive pipeline to load all required ontologies into a dedicated mysql database
+The pipeline will create a database named from current expected release number, load expected ontologies from OLS,
+Check and compute terms closure.
+
+=head1 LICENSE
+    Copyright [2016-2018] EMBL-European Bioinformatics Institute
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+         http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+=head1 CONTACT
+    Please subscribe to the Hive mailing list:  http://listserver.ebi.ac.uk/mailman/listinfo/ehive-users  to discuss Hive-related questions or to be notified of our updates
+=cut
+
+
+package Bio::EnsEMBL::Production::Pipeline::PipeConfig::OntologyMySqlLoad_conf;
+
+use strict;
+use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');
+use warnings FATAL => 'all';
+
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+use Bio::EnsEMBL::Hive::Version 2.4;
+
+sub default_options {
+    my ($self) = @_;
+
+    return {
+        %{$self->SUPER::default_options},
+        ## General parameters
+        'output_dir'    => '/nfs/nobackup/ensembl/' . $self->o('ENV', 'USER') . '/ols_loader/' . $self->o('pipeline_name'),
+        'base_dir'      => $self->o('ENV', 'BASE_DIR'),
+        'srv_cmd'       => undef,
+        'pipeline_name' => 'ols_ontology_loading',
+        'run_mart'      => 1,
+        'wipe_all'      => 1,
+        'verbose'       => 0,
+        'ens_version'=> $self->o('ENV', 'ENS_VERSION'),
+        'db_name'       => "ensembl_ontology_" . $self->o('ens_version'),
+        'mart_db_name'  => 'ontology_mart_' . $self->o('ens_version'),
+        'db_url'        => $self->o('db_host') . $self->o('db_name'),
+        'ontologies'    => [ 'so', 'pato', 'hp', 'vt', 'efo', 'po', 'eo', 'to', 'chebi', 'pr', 'fypo', 'peco', 'bfo',
+            'bto', 'cl', 'cmo', 'eco', 'mp', 'ogms', 'uo' ]
+    }
+}
+
+
+=head2 pipeline_wide_parameters
+=cut
+
+sub pipeline_wide_parameters {
+    my ($self) = @_;
+    return {
+        %{$self->SUPER::pipeline_wide_parameters},
+        'run_mart'     => $self->o('run_mart'),
+        'base_dir'     => $self->o('base_dir'),
+        'db_name'      => $self->o('db_name'),
+        'db_host'      => $self->o('db_host'),
+        'db_url'       => $self->o('db_url'),
+        'ens_version'  => $self->o('ens_version'),
+        'wipe_all'     => $self->o('wipe_all'),
+        'srv'          => $self->o('srv'),
+        'mart_db_name' => $self->o('mart_db_name'),
+    };
+}
+
+
+=head2 pipeline_analyses
+=cut
+
+sub pipeline_analyses {
+    my ($self) = @_;
+    return [
+        {
+            -logic_name      => 'step_init',
+            -input_ids       => [ {} ],
+            -module          => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -meadow_type     => 'LOCAL',
+            -max_retry_count => 1,
+            -flow_into       => {
+                1 => WHEN(
+                    '#wipe_all#' => [ 'reset_db' ],
+                    ELSE [ 'load_go_ontology' ]
+                )
+            },
+        },
+        {
+            -logic_name      => 'reset_db',
+            -module          => 'Bio::EnsEMBL::Hive::RunnableDB::SqlCmd',
+            -meadow_type     => 'LOCAL',
+            -parameters      => {
+                db_conn => $self->o('db_url'),
+                sql     => [ 'DROP DATABASE ' . $self->o('db_name'), 'CREATE DATABASE ' . $self->o('db_name') ]
+            },
+            -flow_into       => [ 'load_go_ontology' ],
+            -max_retry_count => 1,
+        },
+        # load GO ontology first, init_db once and load main ontology
+        {
+            -logic_name        => 'load_go_ontology',
+            -module            => 'bio.ensembl.ontology.OLSHiveLoader',
+            -language          => 'python3',
+            -parameters        => {
+                db_url        => $self->o('db_url'),
+                ontology_name => 'go'
+            },
+            -flow_into         => {
+                1 => [ 'ontologies_factory' ],
+            },
+        }, {
+        -logic_name  => 'ontologies_factory',
+        -module      => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+        -meadow_type => 'LOCAL ',
+        -parameters  => {
+            inputlist    => $self->o('ontologies'),
+            column_names => [ 'ontology_name' ]
+        },
+        -flow_into   => {
+            '2->A' => [ 'ontology_load' ],
+            'A->1' => [ 'compute_closure' ]
+        },
+    },
+        {
+            -logic_name        => 'ontology_load',
+            -module            => 'bio.ensembl.ontology.OLSHiveLoader',
+            -language          => 'python3',
+            -analysis_capacity => 4,
+            -parameters        => {
+                db_url => $self->o('db_url'),
+            },
+            -flow_into         => {
+                1 => [ 'ontology_load' ],
+            },
+        },
+        {
+            -logic_name      => 'compute_closure',
+            -module          => 'Bio::EnsEMBL::Production::Pipeline::OntologiesMySqlLoad::ComputeClosure',
+            -max_retry_count => 1, # use per-analysis limiter
+            -flow_into       => {
+                1 => [ 'add_subset_map' ],
+            },
+        },
+        {
+            -logic_name => 'add_subset_map',
+            -module     => 'Bio::EnsEMBL::Production::Pipeline::OntologiesMySqlLoad::AddSubsetMap',
+            -flow_into  => {
+                1 => WHEN(
+                    '#run_mart#' => [ 'mart_load' ],
+                ),
+            },
+        },
+        {
+            -logic_name => 'mart_load',
+            -module     => 'Bio::EnsEMBL::Production::Pipeline::OntologiesMySqlLoad::MartLoad',
+            -parameters => {
+                mart => $self->o('mart_db_name'),
+                srv  => $self->o('srv')
+            }
+        }
+    ];
+}
+
+1;
