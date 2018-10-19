d8b20a6f076bcd6d9b34bc1b6ac25c647d6a9c62
Thomas Maurel <maurel@ebi.ac.uk>
2018-10-19 15:36:55 +0100

    Remove check on ensrw user as it's not used accross all the divisions

--- modules/Bio/EnsEMBL/Production/Pipeline/Common/AnalysisSetup.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/Common/AnalysisSetup.pm
@@ -225,23 +225,18 @@ sub production_updates {
       $self->param($property, $properties{$property});
      }      
    }
+   $sth = $dbh->prepare(
+    'INSERT IGNORE INTO analysis_web_data '.
+      '(analysis_description_id, web_data_id, species_id, db_type, '.
+        'displayable, created_at, modified_at) '.
+    'SELECT '.
+      'ad.analysis_description_id, ad.default_web_data_id, s.species_id, ?, '.
+        'ad.default_displayable, NOW(), NOW() '.
+    'FROM analysis_description ad, species s '.
+    'WHERE ad.logic_name = ? AND ad.is_current = 1 AND s.db_name = ?;'
+   );
   
-   if ($dbc->user eq 'ensrw') {
-     $sth = $dbh->prepare(
-      'INSERT IGNORE INTO analysis_web_data '.
-        '(analysis_description_id, web_data_id, species_id, db_type, '.
-          'displayable, created_at, modified_at) '.
-      'SELECT '.
-        'ad.analysis_description_id, ad.default_web_data_id, s.species_id, ?, '.
-          'ad.default_displayable, NOW(), NOW() '.
-      'FROM analysis_description ad, species s '.
-      'WHERE ad.logic_name = ? AND ad.is_current = 1 AND s.db_name = ?;'
-    );
-    
-    $sth->execute($db_type, $logic_name, $species);
-  } else {
-    $self->warning("Insufficient permissions to link $species and $logic_name");
-  }
+   $sth->execute($db_type, $logic_name, $species);
   
 }
 
