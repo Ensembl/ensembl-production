90eaa4b80b96c2772eba4ab22ba80c0ce845d9e2
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-01-17 15:38:28 +0000

    Re-added ensembl-taxonomy dependency for tests

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-compara/modules:$PWD/ensembl-datacheck/lib:$PWD/ensembl-variation/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-orm/modules:$PWD/modules
+export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-compara/modules:$PWD/ensembl-datacheck/lib:$PWD/ensembl-variation/modules:$PWD/ensembl-hive/modules:$PWD/ensembl-orm/modules:$PWD/modules:$PWD/ensembl-taxonomy/modules:$PWD/modules
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/htslib
 
