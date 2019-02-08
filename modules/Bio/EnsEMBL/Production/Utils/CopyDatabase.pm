216bec00fe6fce8fed38db7c5f7d367868d4d05b
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-02-08 17:10:20 +0000

    Updated Copyright accepted tests

--- modules/Bio/EnsEMBL/Production/Utils/CopyDatabase.pm
+++ modules/Bio/EnsEMBL/Production/Utils/CopyDatabase.pm
@@ -1,6 +1,7 @@
 =head1 LICENSE
 
-Copyright [2009-2019] EMBL-European Bioinformatics Institute
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2019] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,6 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 =cut
+
 
 package Bio::EnsEMBL::Production::Utils::CopyDatabase;
 
