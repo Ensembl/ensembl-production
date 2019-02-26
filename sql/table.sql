6234af85a5c0f7f885178ce85a823c28dd806c8d
Thomas Maurel <maurel@ebi.ac.uk>
2019-02-26 11:34:58 +0000

    Removed web_data_element table and unused_web_data view. Fixed patch_96_97_d.sql. Fixed and updated table.sql. Removed old production schema prototype mysql workbench and updated current one. Updated production db test schema to match current database.

--- sql/table.sql
+++ sql/table.sql
@@ -32,87 +32,26 @@ CREATE TABLE IF NOT EXISTS meta (
 # Add schema type and schema version to the meta table
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES
   (NULL, 'schema_type', 'production'),
-  (NULL, 'schema_version', 95);
+  (NULL, 'schema_version', 97);
 
 # Patches included in this schema file
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_94_95_a.sql|schema version');
+  VALUES (NULL, 'patch', 'patch_96_97_a.sql|schema version');
 
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_94_95_b.sql|vertebrate_division_rename');
+  VALUES (NULL, 'patch', 'patch_96_97_b.sql|biotype_so_term');
 
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_95_96_b.sql|biotype_so_term');
+  VALUES (NULL, 'patch', 'patch_96_97_c.sql|meta_key_update');
 
--- The 'species' table.
--- Lists the species for which there is a Core database.
-CREATE TABLE species (
-  species_id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  db_name         VARCHAR(255) NOT NULL, -- Name used in database names.
-  common_name     VARCHAR(255) NOT NULL, -- What we often refer to it as.
-  web_name        VARCHAR(255) NOT NULL, -- Name that the web site is using.
-  scientific_name VARCHAR(255) NOT NULL, -- Full name of the species
-  production_name VARCHAR(255) NOT NULL, -- Name that production processes use
-  url_name        VARCHAR(255) NOT NULL DEFAULT '', -- Name that is used in URLs
-  taxon           VARCHAR(8) NOT NULL,
-  species_prefix  VARCHAR(20) NOT NULL,
-  is_current      TINYINT NOT NULL DEFAULT '1',
-  attrib_type_id  SMALLINT(5) UNSIGNED DEFAULT NULL,
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_96_97_d.sql|analysis_description_update');
 
-  -- Columns for the web interface:
-  created_by    INT,
-  created_at    DATETIME,
-  modified_by   INT,
-  modified_at   DATETIME,
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_96_97_e.sql|remove_species');
 
-  PRIMARY KEY (species_id),
-  UNIQUE KEY db_name_idx (db_name),
-  UNIQUE KEY production_name_idx (production_name)
-);
-
--- The 'species_alias' table
--- Lists all aliases for all species including those no longer active
-
-CREATE TABLE species_alias (
-  species_alias_id  INT UNSIGNED NOT NULL AUTO_INCREMENT, -- surrogate key
-  species_id        INT UNSIGNED NOT NULL,      -- FK into species
-  alias             varchar(255) NOT NULL,          -- alias
-  is_current        TINYINT NOT NULL DEFAULT '1',  -- if it's still current
-
-  -- Columns for the web interface:
-  created_by    INT,
-  created_at    DATETIME,
-  modified_by   INT,
-  modified_at   DATETIME,
-
-  PRIMARY KEY (species_alias_id),
-  UNIQUE KEY (alias, is_current),             -- aliases MUST be unique for
-                                                -- the current set. A certain
-                                                -- amount of duplication is
-                                                -- allowed if an alias moved
-                                                -- once
-  KEY sa_speciesid_idx (species_id)
-);
-
-
--- The 'db' table.
--- This table contains all species-specific databases for this release.
-CREATE TABLE db (
-  db_id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  species_id    INT UNSIGNED NOT NULL,  -- FK into 'species'.
-  is_current    TINYINT NOT NULL DEFAULT '0',
-  db_type       ENUM('cdna', 'core', 'coreexpressionatlas',
-                     'coreexpressionest', 'coreexpressiongnf',
-                     'funcgen', 'otherfeatures', 'rnaseq',
-                     'variation', 'vega') NOT NULL DEFAULT 'core',
-  db_release    VARCHAR(8) NOT NULL,
-  db_assembly   VARCHAR(8) NOT NULL,
-  db_suffix     CHAR(1) DEFAULT '',
-  db_host       VARCHAR(32) DEFAULT NULL,
-
-  PRIMARY KEY (db_id),
-  UNIQUE KEY species_release_idx (species_id, db_type, db_release)
-);
+INSERT INTO meta (species_id, meta_key, meta_value)
+  VALUES (NULL, 'patch', 'patch_96_97_f.sql|update_meta_keys');
 
 
 -- The 'master_biotype' table.
@@ -151,27 +90,18 @@ CREATE TABLE meta_key (
   name              VARCHAR(64) NOT NULL,
   is_optional       TINYINT NOT NULL DEFAULT '0',
   is_current        TINYINT NOT NULL DEFAULT '1',
-  db_type           SET('cdna', 'core', 'funcgen', 'otherfeatures',
+  db_type           SET('cdna', 'compara', 'core', 'funcgen', 'otherfeatures',
                         'rnaseq', 'variation', 'vega') NOT NULL DEFAULT 'core',
   description       TEXT,
-
   -- Columns for the web interface:
   created_by    INT,
   created_at    DATETIME,
   modified_by   INT,
   modified_at   DATETIME,
+  is_multi_value tinyint(1) NOT NULL DEFAULT '0',
 
   PRIMARY KEY (meta_key_id),
   UNIQUE KEY name_type_idx (name, db_type)
-);
-
--- The 'meta_key_species' table.
--- Connects the 'meta_key' and the 'species' tables.
-CREATE TABLE meta_key_species (
-  meta_key_id       INT UNSIGNED NOT NULL,
-  species_id        INT UNSIGNED NOT NULL,
-
-  UNIQUE KEY uniq_idx (meta_key_id, species_id)
 );
 
 -- The 'analysis_description' table.
@@ -185,8 +115,8 @@ CREATE TABLE analysis_description (
   display_label             VARCHAR(256) NOT NULL,
   db_version                TINYINT(1) NOT NULL DEFAULT '1',
   is_current                TINYINT NOT NULL DEFAULT '1',
-  default_web_data_id       INT(10) UNSIGNED DEFAULT NULL,
-  default_displayable       TINYINT DEFAULT NULL,
+  web_data_id       INT(1)  DEFAULT NULL,
+  displayable       TINYINT(1) NOT NULL,
 
   -- Columns for the web interface:
   created_by    INT,
@@ -198,34 +128,6 @@ CREATE TABLE analysis_description (
   UNIQUE KEY logic_name_idx (logic_name)
 );
 
--- The 'analysis_web_data' table.
--- Many-to-many connection table.
--- Contains the data for the 'displayable' columns in the
--- 'analysis_description' table.  Ties together species,
--- analysis_description, and the web_data.
-CREATE TABLE analysis_web_data (
-  analysis_web_data_id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  analysis_description_id   INT UNSIGNED NOT NULL,
-  web_data_id               INT UNSIGNED DEFAULT NULL,
-  species_id                INT UNSIGNED NOT NULL,
-
-  db_type                   ENUM('cdna', 'core', 'funcgen',
-                                'otherfeatures', 'rnaseq', 'vega',
-                                'presite', 'sangervega', 'grch37_archive') NOT NULL DEFAULT 'core',
-
-  displayable               TINYINT NOT NULL DEFAULT '1',
-
-  -- Columns for the web interface:
-  created_by    INT,
-  created_at    DATETIME,
-  modified_by   INT,
-  modified_at   DATETIME,
-
-  PRIMARY KEY (analysis_web_data_id),
-  UNIQUE KEY uniq_idx (species_id, db_type, analysis_description_id),
-  KEY ad_idx (analysis_description_id),
-  KEY wd_idx (web_data_id)
-);
 
 -- The 'web_data' table.
 -- Contains the unique web_data.
@@ -241,6 +143,7 @@ CREATE TABLE web_data (
   created_at    DATETIME,
   modified_by   INT,
   modified_at   DATETIME,
+  description varchar(255) DEFAULT NULL,
 
   PRIMARY KEY (web_data_id)
 );
@@ -362,148 +265,7 @@ CREATE TABLE master_unmapped_reason (
   PRIMARY KEY (unmapped_reason_id)
 );
 
-
--- The 'changelog' and 'changelog_species' tables
--- Used for web purposes to store release related information
-CREATE TABLE changelog (
-  changelog_id              INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  release_id                INT(11) DEFAULT NULL,
-  title                     VARCHAR(128) DEFAULT NULL,
-  content                   TEXT,
-  notes                     TEXT,
-  status                    ENUM('declared','handed_over','postponed','cancelled') NOT NULL DEFAULT 'declared',
-  team                      ENUM('Compara','Core','Funcgen','Genebuild','Outreach','Variation','Web','EnsemblGenomes','Wormbase','Production') DEFAULT NULL,
-  assembly                  ENUM('N','Y') NOT NULL DEFAULT 'N',
-  gene_set                  ENUM('N','Y') NOT NULL DEFAULT 'N',
-  repeat_masking            ENUM('N','Y') NOT NULL DEFAULT 'N',
-  stable_id_mapping         ENUM('N','Y') NOT NULL DEFAULT 'N',
-  affy_mapping              ENUM('N','Y') NOT NULL DEFAULT 'N',
-  biomart_affected          ENUM('N','Y') NOT NULL DEFAULT 'N',
-  variation_pos_changed     ENUM('N','Y') NOT NULL DEFAULT 'N',
-  mitochondrion             ENUM('N','Y', 'changed') NOT NULL DEFAULT 'N',
-  db_status                 ENUM('N/A','unchanged','patched','new') NOT NULL DEFAULT 'N/A',
-  db_type_affected          SET('cdna','core','funcgen','otherfeatures','rnaseq','variation','vega','presite','sangervega') DEFAULT NULL,
-  priority                  TINYINT(1) NOT NULL DEFAULT '2',
-  is_current                TINYINT(1) NOT NULL DEFAULT '1',
-
-  -- Columns for the web interface:
-  created_by    INT,
-  created_at    DATETIME,
-  modified_by   INT,
-  modified_at   DATETIME,
-
-  PRIMARY KEY (changelog_id)
-);
-
--- Connects the 'changelog' and the 'species' tables.
-CREATE TABLE changelog_species (
-  changelog_id              INT(11) NOT NULL DEFAULT '0',
-  species_id                INT(11) NOT NULL DEFAULT '0',
-
-  PRIMARY KEY (changelog_id,species_id)
-);
-
--- Tables to store species divisions
-CREATE TABLE division (
-  division_id           int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name                  VARCHAR(32) NOT NULL,
-  shortname             VARCHAR(4) NOT NULL,
-
-  PRIMARY KEY (division_id),
-  UNIQUE KEY name_idx (name),
-  UNIQUE KEY shortname_idx (shortname)
-) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
-
-CREATE TABLE division_species (
-  division_id           INT(10) DEFAULT NULL,
-  species_id            INT(10) DEFAULT NULL,
-
-  UNIQUE KEY division_species_idx (division_id,species_id)
-) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
-
-CREATE TABLE division_db (
-  division_id           INT(10) DEFAULT NULL,
-  db_name               VARCHAR(64) NOT NULL,
-  db_type               ENUM('COMPARA','GENE_MART','SEQ_MART','SNP_MART','FEATURES_MART','ONTOLOGY_MART','ONTOLOGY','TAXONOMY','ANCESTRAL','WEBSITE','INFO') NOT NULL,
-  is_current            TINYINT(1) NOT NULL DEFAULT '1',
-  update_type           ENUM('NEW_GENOME','NEW_ASSEMBLY','NEW_GENEBUILD','PATCHED','OTHER') DEFAULT 'PATCHED',
-  release_status        ENUM('NOT_READY','COMPARA_READY','WEB_READY') DEFAULT 'NOT_READY',
-
-  UNIQUE KEY division_db_idx (division_id,db_name,is_current)
-) COLLATE=latin1_swedish_ci ENGINE=MyISAM;
-
 -- VIEWS
-
--- The 'db_list' view provides the full database names for all databases
--- in the 'db' table that are current.
-CREATE DEFINER = CURRENT_USER SQL SECURITY INVOKER VIEW db_list AS
-SELECT  db_id AS db_id,
-        CONCAT(
-          CONCAT_WS('_', db_name, db_type, db_release, db_assembly),
-        db_suffix) AS full_db_name
-FROM    species
-  JOIN  db USING (species_id)
-WHERE species.is_current = true;
-
--- The 'full_analysis_description' view provides, for each database,
--- /nearly/ exactly what should go into the 'analysis_description'
--- table, apart from the fact that it uses 'analysis.logic_name' rather
--- than 'analysis.analysis_id'.
-CREATE DEFINER = CURRENT_USER SQL SECURITY INVOKER VIEW full_analysis_description AS
-SELECT  list.full_db_name AS full_db_name,
-        ad.logic_name AS logic_name,
-        ad.description AS description,
-        ad.display_label AS display_label,
-        awd.displayable AS displayable,
-        wd.data AS web_data
-FROM db_list list
-  JOIN db USING (db_id)
-  JOIN analysis_web_data awd
-    ON ( db.species_id = awd.species_id
-    AND  db.db_type = awd.db_type )
-  JOIN analysis_description ad USING (analysis_description_id)
-  LEFT JOIN web_data wd USING (web_data_id)
-WHERE   db.is_current = true
-  AND   ad.is_current = true;
-
--- The 'logic_name_overview' is a helper view for people trying to
--- make sense of the 'analysis_description', 'analysis_web_data', and
--- 'web_data' tables.
-CREATE DEFINER = CURRENT_USER SQL SECURITY INVOKER VIEW logic_name_overview AS
-SELECT
-  awd.analysis_web_data_id AS analysis_web_data_id,
-  ad.logic_name AS logic_name,
-  ad.analysis_description_id AS analysis_description_id,
-  s.db_name AS species,
-  s.species_id AS species_id,
-  awd.db_type AS db_type,
-  wd.web_data_id AS web_data_id,
-  awd.displayable AS displayable
-FROM   analysis_description ad
-  JOIN analysis_web_data awd USING (analysis_description_id)
-  JOIN species s USING (species_id)
-  LEFT JOIN web_data wd USING (web_data_id)
-WHERE   s.is_current = true
-  AND   ad.is_current = true;
-
--- The 'unconnected_analyses' view gives back the analyses from
--- 'analysis_description' that are unused in 'analysis_web_data'.
-CREATE DEFINER = CURRENT_USER SQL SECURITY INVOKER VIEW unconnected_analyses AS
-SELECT  ad.analysis_description_id AS analysis_description_id,
-        ad.logic_name AS logic_name
-FROM    analysis_description ad
-  LEFT JOIN analysis_web_data awd USING (analysis_description_id)
-WHERE   awd.species_id IS NULL
-  AND   ad.is_current = true;
-
--- The 'unused_web_data' view gives back the entries in 'web_data' that
--- are not connected to any analysis in 'analysis_web_data'.
-CREATE DEFINER = CURRENT_USER SQL SECURITY INVOKER VIEW unused_web_data AS
-SELECT  wd.web_data_id
-FROM    web_data wd
-  LEFT JOIN analysis_web_data awd USING (web_data_id)
-WHERE   awd.analysis_web_data_id IS NULL;
-
 
 -- Views for the master tables.  These views are simply selecting the
 -- entries from the corresponding master table that have is_current
@@ -546,7 +308,7 @@ SELECT
   description AS description,
   biotype_group AS biotype_group,
   so_acc AS so_acc,
-  so_term AS so_term,
+  so_term AS so_term
 FROM master_biotype
 WHERE is_current = true
 ORDER BY biotype_id;
