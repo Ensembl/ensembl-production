f69869a7c86773f0ef351c4715d978ea7deb6be3
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-04-01 09:37:16 +0100

    Update OLSLoadAll_conf.pm
    Remove MOD from loaded list (it's only in allowed_ontologies for testing purpose)

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OLSLoadAll_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/OLSLoadAll_conf.pm
@@ -54,7 +54,7 @@ sub default_options {
         %{$self->SUPER::default_options},
         'wipe_all'   => 1,
         'ontologies'    => ['GO', 'SO', 'PATO', 'HP', 'VT', 'EFO', 'PO', 'EO', 'TO', 'CHEBI', 'PR', 'FYPO', 'PECO', 'BFO',
-                          'BTO', 'CL', 'CMO', 'ECO', 'MOD', 'MP', 'OGMS', 'UO', 'MONDO', 'PHI']
+                          'BTO', 'CL', 'CMO', 'ECO', 'MP', 'OGMS', 'UO', 'MONDO', 'PHI']
     }
 }
 
