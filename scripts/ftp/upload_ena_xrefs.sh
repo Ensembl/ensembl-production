991c2caf741f7e267ddbb8df88c325448c477fbd
Amonida Zadissa <amonida@ebi.ac.uk>
2019-04-26 11:31:57 +0100

    added the boilerplate licence text

--- scripts/ftp/upload_ena_xrefs.sh
+++ scripts/ftp/upload_ena_xrefs.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+# Copyright [2019] EMBL-European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 base_dir=$1
 
-if [ -z "$base_dir" ]; then                                                                                                                                                                                         
-    echo "Usage: $0 <path to FTP directory>" 2>&1                                                                                                                                                                                 
-    exit 1                                                                                                                                                                                                                                 
-fi       
+if [ -z "$base_dir" ]; then
+    echo "Usage: $0 <path to FTP directory>" 2>&1
+    exit 1
+fi
 
 if [ ! -d "$base_dir" ]; then
     echo "Dump directory $base_dir not found" 2>&1
     exit 2
-fi 
+fi
 
 function upload_file {
     file=$(basename $1)
