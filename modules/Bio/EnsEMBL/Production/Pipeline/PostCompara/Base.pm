df5265ec5a8ec5c86bbff127c581b7e0098ede97
James Allen <jallen@ebi.ac.uk>
2019-08-19 12:24:35 +0100

    Removing unused functions

--- modules/Bio/EnsEMBL/Production/Pipeline/PostCompara/Base.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PostCompara/Base.pm
@@ -15,28 +15,14 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-=head1 NAME
-
-Bio::EnsEMBL::Production::Pipeline::PostCompara::Base
-
-=cut
-
-=head1 DESCRIPTION
-
-=head1 AUTHOR
-
-ckong
-
 =cut
 package Bio::EnsEMBL::Production::Pipeline::PostCompara::Base;
 
 use strict;
 use warnings;
-use Data::Dumper;
+
 use Bio::EnsEMBL::Registry;
-use Bio::EnsEMBL::Taxonomy::DBSQL::TaxonomyNodeAdaptor;
-use Bio::EnsEMBL::Utils::SqlHelper;
-use Bio::EnsEMBL::Attribute;
+
 use base ('Bio::EnsEMBL::Production::Pipeline::Common::Base');
 
 =head2 fetch_homologies 
@@ -113,10 +99,6 @@ sub fetch_homologies {
 return \%homology_cache;
 }
 
-=head2 homology_type_allowed
-
-  
-=cut
 sub homology_type_allowed {
     my $h                      = shift;
     my $homology_types_allowed = shift;
@@ -128,36 +110,6 @@ sub homology_type_allowed {
 return undef;
 }
 
-=head2 get_taxon_ancestry
-
-  
-=cut
-sub get_taxon_ancestry {
-    my $self        = shift;
-    my $to_taxon_id = shift;
-
-    my $taxonomy_db            = Bio::EnsEMBL::Registry->get_DBAdaptor('multi','taxonomy');
-    my $node_adaptor = $taxonomy_db->get_TaxonomyNodeAdaptor();
-
-    my $node         = $node_adaptor->fetch_by_taxon_id($to_taxon_id);
-    my @lineage      = @{$node_adaptor->fetch_ancestors($node)};
-    my @ancestors;
-    my @names;
-    push @ancestors, $to_taxon_id;
-
-    for my $node (@lineage) {
-       push @ancestors, $node->taxon_id();
-       push @names, $node->names()->{'scientific name'}->[0];
-       #print STDERR  "\t\tNode ".$node->taxon_id()." is ".$node->rank()." ".$node->names()->{'scientific name'}->[0]."\n";
-    }
-
-return (\@ancestors,\@names);
-}
-
-=head2 check_directory
-
-  
-=cut
 sub check_directory {
     my ($self, $dir) = @_;
 
@@ -168,40 +120,6 @@ sub check_directory {
     }
 
 return;
-}
-
-
-=head2 print_GOstats
-
-  
-=cut
-sub print_GOstats {
-    my $self   = shift;
-    my $to_ga  = shift;
-    my $data   = shift;
- 
-    my $total_genes = count_rows($to_ga, "SELECT COUNT(*) FROM gene g");
-    my $count;
-    print $data "\tUnique GO terms total:";
-    print $data &count_rows($to_ga, "SELECT COUNT(DISTINCT(x.dbprimary_acc)) FROM xref x, external_db e WHERE e.external_db_id=x.external_db_id AND e.db_name='GO'");
-    print $data " Unique GO terms from projection:";
-    print $data &count_rows($to_ga, "SELECT COUNT(DISTINCT(x.dbprimary_acc)) FROM xref x, external_db e WHERE e.external_db_id=x.external_db_id AND e.db_name='GO' AND x.info_type='PROJECTION'");
-    print $data "\n\n";
-
-return 0;
-}
-
-=head2 count_rows
-
-  
-=cut
-sub count_rows {
-    my ($adaptor, $sql) = @_;
-
-    my $sth = $adaptor->dbc->prepare($sql);
-    $sth->execute();
-
-return ($sth->fetchrow_array())[0];
 }
 
 1;
