a45ce0950d9156199d5ee87f937d38a58a2868b7
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2020-01-07 14:01:56 +0000

    Yearly copyright update

--- scripts/rdf/void_rdf.py
+++ scripts/rdf/void_rdf.py
@@ -2,7 +2,7 @@
 """Class whose instances take information from project species RDF files,
 construct an RDF graph representing the dataset (VOID), and dump it to file."""
 # Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-# Copyright [2016-2019] EMBL-European Bioinformatics Institute
+# Copyright [2016-2020] EMBL-European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
