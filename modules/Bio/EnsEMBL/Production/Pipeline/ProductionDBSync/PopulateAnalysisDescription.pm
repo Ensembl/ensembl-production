eb3651ba0a1acb8ba77102694fa4a8d49fac3fd0
vinay-ebi <vinay@ebi.ac.uk>
2020-02-28 17:03:58 +0000

    LICENSE year changed

--- modules/Bio/EnsEMBL/Production/Pipeline/ProductionDBSync/PopulateAnalysisDescription.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/ProductionDBSync/PopulateAnalysisDescription.pm
@@ -1,7 +1,5 @@
 =head1 LICENSE
-
-Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
-Copyright [2016-2020] EMBL-European Bioinformatics Institute
+Copyright [2018-2020] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,6 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+=head1 NAME
+Bio::EnsEMBL::Production::Pipeline::ProductionDBSync::PopulateAnalysisDescription
+
+=head1 DESCRIPTION
+A Module for synchronising analysis description tables
 =cut
 
 package Bio::EnsEMBL::Production::Pipeline::ProductionDBSync::PopulateAnalysisDescription;
