2c6e023400dd945b41efd378e9075d8d7b75a36d
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2020-07-06 14:50:17 +0100

    Added output to either "-species production_name" (For hive) or "dbname1,dbname2" (for dbcopy). Added parameter to either output to file / standard output (can then be used in pipeline config script)

--- scripts/py/meta_hive_species.py
+++ scripts/py/meta_hive_species.py
@@ -32,16 +32,18 @@ from os.path import isdir
 from os.path import join, isfile, realpath
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
-
+logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser(
     description='Parse outputs from `~ensemble-metadata/misc_scripts/genomes_report.pl to get species_list for hive'
                 ' init_pipeline command')
 parser.add_argument('-i', '--input_dir', type=str, required=True, help='Input directory')
 parser.add_argument('-d', '--division', help='Restrict to a single division', type=str)
-parser.add_argument('-f', '--output_name', help='Overwrite default output file name', required=False, type=str,
-                    default='species.txt')
+parser.add_argument('-f', '--output_file', help='Overwrite default output file name', required=False,
+                    action='store_true')
 parser.add_argument('-t', '--genomes_types', help='List genomes events (n,r,ua,ug)', required=False, type=str,
                     default='n,r,ua,ug')
+parser.add_argument('-o', '--output', help='species,dbname', required=False, type=str,
+                    default='species')
 
 divisions = (
     'vertebrates',
@@ -51,48 +53,56 @@ divisions = (
     'plants'
 )
 names = {
-   'n': '{}-new_genomes.txt',
-   'r': '{}-renamed_genomes.txt',
-   'ua': '{}-updated_annotations.txt',
-   'ug': '{}-updated_assemblies.txt',
+    'n': '{}-new_genomes.txt',
+    'r': '{}-renamed_genomes.txt',
+    'ua': '{}-updated_annotations.txt',
+    'ug': '{}-updated_assemblies.txt',
 }
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    if not args.output_file:
+        logger.setLevel(logging.ERROR)
     if args.division:
         assert args.division in divisions, "Unknown Division name, please chose in {}".format(divisions)
-        logging.info("Process only division {}".format(args.division))
+        logger.info("Process only division {}".format(args.division))
         divisions = [args.division]
     else:
-        logging.info("Process all divisions")
+        logger.info("Process all divisions")
 
     home_dir = getenv("HOME")
     assert isdir(args.input_dir), "Input dir is mandatory when not running report_genomes.pl"
     scan_dir = realpath(args.input_dir)
-    logging.info("Input Dir: {}".format(realpath(scan_dir)))
+    logger.info("Input Dir: {}".format(realpath(scan_dir)))
 
     species = []
     expected_files_names = [names[n] for n in args.genomes_types.split(',')]
 
     for div in divisions:
-        logging.info("Division: {}".format(div))
-        expected_files = [ ex.format(div) for ex in expected_files_names]
+        logger.info("Division: {}".format(div))
+        expected_files = [ex.format(div) for ex in expected_files_names]
         for file in expected_files:
             file_path = join(scan_dir, file)
             if isfile(file_path):
                 with open(file_path, 'r') as f:
-                    logging.info("Current File: {}".format(f.name))
+                    logger.info("Current File: {}".format(f.name))
                     reader = csv.reader(f, delimiter="\t")
                     next(reader)
                     for row in reader:
-                        species.append(row[0])
+                        species.append(row[0]) if args.output == 'species' else species.append(row[2])
             else:
-                logging.info("File not found: {}".format(file_path))
+                logger.info("File not found: {}".format(file_path))
 
-    logging.info("Retrieved species:")
-    logging.info(species)
-    out_file_path = join(home_dir, args.output_name)
-    with open(out_file_path, 'w') as f:
+    logger.info("Retrieved species:")
+    logger.info(species)
+    if args.output_file:
+        out_file_path = join(home_dir, args.output_file)
+        with open(out_file_path, 'w') as f:
+            for spec in species:
+                f.write('-species {} '.format(spec)) if args.output == 'species' else f.write('{},'.format(spec))
+        logger.info("File generated in {}".format(out_file_path))
+    else:
         for spec in species:
-            f.write('-species {} '.format(spec))
-    logging.info("File generated in {}".format(out_file_path))
+            print('-species {} '.format(spec), end="", flush=True) if args.output == 'species' else print(
+                '{},'.format(spec), end="", flush=True)
+
