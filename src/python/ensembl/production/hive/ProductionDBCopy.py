c6ccc4e0cd69d0dde2bb79bf25b9cb2f73581668
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2020-11-23 18:13:53 +0000

    Replaced CopyDatabaseHive module in copy pipeline with ProductionDBCopy Rest Client.

--- src/python/ensembl/production/hive/ProductionDBCopy.py
+++ src/python/ensembl/production/hive/ProductionDBCopy.py
@@ -12,12 +12,26 @@
    limitations under the License.
 """
 import time
+from urllib.parse import urlparse
 
 from ensembl.hive.HiveRESTClient import HiveRESTClient
 
 
 class ProductionDBCopy(HiveRESTClient):
     """ OLS MySQL loader: initialise basics info in Ontology DB """
+
+    def fetch_input(self):
+        src_db = urlparse(self.param('source_db_uri'))
+        tgt_db = urlparse(self.param('target_db_uri'))
+        if src_db.scheme and src_db.hostname and tgt_db.hostname and tgt_db.scheme:
+            # only if both parameters are set and valid
+            self.param('payload', {
+                "src_host": ':'.join((src_db.hostname, src_db.port)),
+                "src_incl_db": src_db.path[1:],
+                "tgt_host": tgt_db.hostname,
+                "tgt_db_name": src_db.path[1:],
+                "user": "ensprod"
+            })
 
     def run(self):
         if self.response.status_code != 201:
