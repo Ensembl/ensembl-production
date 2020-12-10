569b5c05582a4fac5346842594e4b20b93ba417b
James Allen <jallen@ebi.ac.uk>
2020-12-10 14:32:25 +0000

    Attach summaries of stable ID counts to email, for sanity checking. Also provide a summary of the duplicate status, and don't bother checking otherfeatures dbs for duplicates.

--- modules/Bio/EnsEMBL/Production/Pipeline/StableID/EmailReport.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/StableID/EmailReport.pm
@@ -15,7 +15,7 @@ limitations under the License.
 Bio::EnsEMBL::Production::Pipeline::StableID::EmailReport
 
 =head1 DESCRIPTION
-Send an email with details of any non-unique stable IDs.
+Send an email with a summary and details of any non-unique stable IDs.
 
 =cut
 
@@ -39,21 +39,62 @@ sub fetch_input {
   $output_dir = catdir($output_dir, $pipeline_name);
   path($output_dir)->mkpath();
 
-  my $duplicates = $self->batch_duplicates($output_dir);
+  my $core_count = $self->summary($output_dir, 'core');
+  my $of_count   = $self->summary($output_dir, 'otherfeatures');
+  my ($duplicates, $species) = $self->batch_duplicates($output_dir, 'core');
 
   my $subject = "Stable ID pipeline completed ($pipeline_name)";
   $self->param('subject', $subject);
 
   my $text =
     "The $pipeline_name pipeline has completed successfully.\n\n".
-    "There are $duplicates duplicated stable IDs. ".
-    "Details: $output_dir";
+    "There are stable IDs for $core_count core databases and ".
+    "$of_count otherfeatures databases.\n\n".
+    "There are $duplicates duplicated stable IDs ".
+    "across ".scalar(keys(%$species))." species groups.\n\n".
+    "Summary files are attached; for detailed duplicate ".
+    "information, see: $output_dir";
 
   $self->param('text', $text);
 }
 
+sub summary {
+  my ($self, $output_dir, $db_type) = @_;
+
+  my $count = 0;
+
+  my $output_file = catdir($output_dir, "summary_${db_type}.txt");
+
+  my $dbh = $self->data_dbc->db_handle;
+  my $sql = qq/
+    SELECT
+      name, COUNT(*) AS id_count
+    FROM
+      stable_id_lookup INNER JOIN
+      species USING (species_id)
+    WHERE db_type = '$db_type'
+    GROUP BY name
+    ORDER BY name
+  /;
+
+  my $sth = $dbh->prepare($sql) or die $dbh->errstr();
+  $sth->execute();
+
+  my $out = path($output_file);
+  $out->remove if -e $output_file;
+
+  while (my @row = $sth->fetchrow_array) {
+    $out->append_raw(join("\t",@row)."\n");
+    $count++;
+  }
+
+  push @{$self->param('attachments')}, $output_file;
+
+  return $count;
+}
+
 sub batch_duplicates {
-  my ($self, $output_dir) = @_;
+  my ($self, $output_dir, $db_type) = @_;
 
   # Looking for duplicates across all stable IDs creates vast
   # temporary tables and we run out of space on the server.
@@ -61,17 +102,28 @@ sub batch_duplicates {
   # distributed, but it does the job and is easy to implement.
 
   my $duplicates = 0;
+  my $species = {};
   my @initials = ("A".."Z");
   foreach my $initial (@initials) {
-    my $output_file = catdir($output_dir, "$initial.txt");
-    $duplicates += $self->find_duplicates($initial, $output_file);
+    my $output_file = catdir($output_dir, "${initial}_${db_type}.txt");
+    $duplicates += $self->find_duplicates($initial, $output_file, $db_type, $species);
   }
 
-  return $duplicates;
+  my $summary_file = catdir($output_dir, "duplicates_${db_type}.txt");
+  my $summary = path($summary_file);
+  $summary->remove if -e $summary_file;
+
+  foreach (sort keys %$species) {
+    $summary->append_raw($_."\t".$$species{$_}."\n");
+  }
+
+  push @{$self->param('attachments')}, $summary_file;
+
+  return ($duplicates, $species);
 }
 
 sub find_duplicates {
-  my ($self, $initial, $output_file) = @_;
+  my ($self, $initial, $output_file, $db_type, $species) = @_;
 
   my $duplicates = 0;
 
@@ -85,6 +137,7 @@ sub find_duplicates {
       stable_id_lookup INNER JOIN
       species USING (species_id)
     WHERE
+      db_type = '$db_type' AND
       stable_id LIKE '${initial}%'
     GROUP BY
       stable_id, db_type, object_type
@@ -99,6 +152,7 @@ sub find_duplicates {
 
   while (my @row = $sth->fetchrow_array) {
     $out->append_raw(join("\t",@row)."\n");
+    $$species{$row[4]}++;
     $duplicates++;
   }
 
