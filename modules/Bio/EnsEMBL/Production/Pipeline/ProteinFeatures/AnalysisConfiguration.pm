d5f27d388ffd3fca0ed3e214709152001cb80631
James Allen <jallen@ebi.ac.uk>
2021-01-08 12:12:08 +0000

    Merge the modules that iterated over InterPro sources and analyses, to enable version checking, and subsequent execution for a subset of sources.

--- modules/Bio/EnsEMBL/Production/Pipeline/ProteinFeatures/AnalysisConfiguration.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/ProteinFeatures/AnalysisConfiguration.pm
@@ -17,7 +17,7 @@ limitations under the License.
 
 =cut
 
-package Bio::EnsEMBL::Production::Pipeline::ProteinFeatures::InterProScanPrograms;
+package Bio::EnsEMBL::Production::Pipeline::ProteinFeatures::AnalysisConfiguration;
 
 use strict;
 use warnings;
@@ -25,37 +25,92 @@ use base ('Bio::EnsEMBL::Production::Pipeline::Common::Base');
 
 sub run {
   my ($self) = @_;
-  my $analyses = $self->param_required('analyses');
-  
-  my @all_applications;
-  my @lookup_applications;
-  my @nolookup_applications;
-  
-  foreach my $analysis (@{$analyses}) {
-    if (exists $$analysis{'ipscan_name'}) {
-      if (exists $$analysis{'ipscan_lookup'} && $$analysis{'ipscan_lookup'}) {
-        push @lookup_applications, $$analysis{'ipscan_name'};
+  my $analyses             = $self->param_required('analyses');
+  my $interproscan_version = $self->param_required('interproscan_version');
+  my $check_db_version     = $self->param_required('check_interpro_db_version');
+  my $run_seg              = $self->param_required('run_seg');
+
+  my $aa = $self->core_dba->get_adaptor('Analysis');
+
+  my $filtered_analyses = [];
+  foreach my $analysis_config (@{$analyses}) {
+    # If the analysis doesn't already exist, then always need to create it.
+    # For InterPro sources with the current InterProScan version, and for seg,
+    # don't re-annotate - it would only reproduce exactly the same results.
+    # If 'check_db_version' is true, then we only annotate if the InterPro
+    # source database version has changed (InterPro are adding entries with
+    # every release, so this leads to slightly less comprehensive annotation,
+    # but handling large data volumes would not be possible otherwise).
+    # We always delete all GO and reannotate; and we want to keep existing
+    # pathway data (the code is clever enough to remove pathways attached
+    # to any InterPro features that get removed).
+    my $logic_name = $$analysis_config{'logic_name'};
+    my $analysis = $aa->fetch_by_logic_name($logic_name);
+
+    if (defined($analysis)) {
+      if ($logic_name eq 'seg') {
+        $run_seg = 0;
+        $self->warning("Skipping $logic_name - annotation exists");
+      } elsif ($logic_name eq 'interpro2go') {
+        push @$filtered_analyses, $analysis_config;
+      } elsif ($logic_name eq 'interpro2pathway') {
+        $self->warning("Re-using existing pathway analysis");
+      } elsif ($analysis->program_version eq $interproscan_version) {
+        $self->warning("Skipping $logic_name - annotation exists for InterProScan $interproscan_version");
+      } elsif ($check_db_version && ($$analysis_config{'db_version'} eq $analysis->db_version)) {
+        $self->warning("Skipping $logic_name - annotation exists for db version ".$analysis->db_version);
       } else {
-        push @nolookup_applications, $$analysis{'ipscan_name'};
+        push @$filtered_analyses, $analysis_config;
+      }
+    } else {
+      if ($logic_name ne 'seg' || $run_seg) {
+        push @$filtered_analyses, $analysis_config;
       }
     }
   }
-  @all_applications = (@lookup_applications, @nolookup_applications);
-  
-  $self->param(
-    'interproscan_applications',
-    {
-      'interproscan_lookup_applications'   => \@lookup_applications,
-      'interproscan_nolookup_applications' => \@nolookup_applications,
-      'interproscan_local_applications'    => \@all_applications,
+
+  my @all;
+  my @lookup;
+  my @nolookup;
+
+  foreach my $analysis_config (@{$filtered_analyses}) {
+    if (exists $$analysis_config{'ipscan_name'}) {
+      if (exists $$analysis_config{'ipscan_lookup'} && $$analysis_config{'ipscan_lookup'}) {
+        push @lookup, $$analysis_config{'ipscan_name'};
+      } else {
+        push @nolookup, $$analysis_config{'ipscan_name'};
+      }
     }
-  );
+  }
+  @all = (@lookup, @nolookup);
+
+  $self->param('filtered_analyses', $filtered_analyses);
+  $self->param('run_seg', $run_seg);
+  $self->param('lookup', \@lookup);
+  $self->param('nolookup', \@nolookup);
+  $self->param('all', \@all);
 }
 
 sub write_output {
   my ($self) = @_;
-  
-  $self->dataflow_output_id($self->param('interproscan_applications'), 1 );
+
+  my $analyses = $self->param('filtered_analyses');
+  my $run_seg  = $self->param('run_seg');
+  my $lookup   = $self->param('lookup');
+  my $nolookup = $self->param('nolookup');
+  my $all      = $self->param('all');
+
+  if ( $run_seg || scalar(@$all) ) {
+    $self->dataflow_output_id( $analyses, 2 );
+
+    $self->dataflow_output_id(
+      {
+        run_seg                            => $run_seg,
+        interproscan_lookup_applications   => $lookup,
+        interproscan_nolookup_applications => $nolookup,
+        interproscan_local_applications    => $all,
+      }, 3 );
+  }
 }
 
 1;
