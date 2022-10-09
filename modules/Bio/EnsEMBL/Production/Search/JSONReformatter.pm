27aa91d0eb0ed4a922ee44fc050c99bd72e1d920
vinay-ebi <vinay@ebi.ac.uk>
2022-10-09 17:15:26 +0100

    check json error

--- modules/Bio/EnsEMBL/Production/Search/JSONReformatter.pm
+++ modules/Bio/EnsEMBL/Production/Search/JSONReformatter.pm
@@ -38,7 +38,7 @@ our @EXPORT = qw(process_json_file reformat_json);
 
 use Log::Log4perl qw/get_logger/;
 use Carp qw/croak/;
-#BEGIN { $ENV{PERL_JSON_BACKEND} = 'JSON::XS' }
+BEGIN { $ENV{PERL_JSON_BACKEND} = 'JSON::PP' }
 
 use JSON;
 
@@ -66,7 +66,7 @@ sub process_json_file {
   {
     local $/ = '}';
     while (<$fh>) {
-     print("...///$_\n");	    
+	    #print("...///$_\n");	    
       my $obj = $json->incr_parse($_);
       if ( defined $obj ) {
         $n++;
