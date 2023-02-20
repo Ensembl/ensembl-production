ddb99354c5e66dd11ae4f8f6dcb3b296159462bd
Tamara El Naboulsi <ten@codon-login-02.ebi.ac.uk>
2023-02-20 10:02:36 +0000

    New DataFile Scanner pipeline

--- src/python/ensembl/production/datafile/scraper/serializers.py
+++ src/python/ensembl/production/datafile/scraper/serializers.py
@@ -16,6 +16,7 @@ from typing import Optional, Tuple
 
 from .utils import get_metadata_from_db, ManifestRow
 
+import re
 
 def metadata_from_manifest(manifest_row: ManifestRow, manifest_dir: Path):
     data = manifest_row._asdict()
@@ -32,3 +33,62 @@ def metadata_from_db(
     data = ens_metadatas[0]._asdict()
     data["release_date"] = data["release_date"].isoformat()
     return data, None
+
+def metadata_from_filename(file_name: str, file_path: str, release: int):
+    file_suffixes = Path(file_name).suffixes
+    path_array = file_path.split("/")
+
+    # Get file extension
+    if len(file_suffixes) == 0:
+        return None, None, None
+    valid_extensions = ["gff3", "gtf", "fa", "bam", "dat"]
+
+    # Try last extension, if not valid then try next to last (in case of compression)
+    ext = file_suffixes[-1]
+    ext = ext[1:]
+    if len(file_suffixes) > 1 and ext not in valid_extensions:
+      ext = file_suffixes[-2]
+      ext = ext[1:]
+    if ext not in valid_extensions:
+      return None, None, None
+
+    # Get file format
+    format = ext
+    if ext == "fa":
+      format = "fasta"
+    elif ext == "bam":
+      format = "bamcov"
+    elif ext == "dat":
+      if "embl" in path_array:
+        format = "embl"
+      elif "genbank" in path_array:
+        format = "genbank"
+
+    content_type = ''
+    if format != "bamcov":
+      filenames_regex = re.compile(
+        (
+          r"^(?P<species>\w+)\.(?P<assembly>[\w\-\.]+?)\.(?P<release>(5|1)\d{1,2})\."
+          r"(?P<content_type>abinitio|chr|chr_patch_hapl_scaff|nonchromosomal|(chromosome|plasmid|scaffold)\.[\w\-\.]+?|primary_assembly[\w\-\.]*?|(chromosome_)?group\.\w+?)?.*?"
+        )
+      )
+      if format == "fasta":
+        filenames_regex = re.compile(
+            (
+                r"^(?P<species>\w+)\.(?P<assembly>[\w\-\.]+?)\.(?P<sequence_type>dna(_sm|_rm)?|cdna|cds|pep|ncrna)\."
+                r"(?P<content_type>abinitio|all|alt|toplevel|nonchromosomal|(chromosome|plasmid|scaffold)\.[\w\-\.]+?|primary_assembly[\w\-\.]*?|(chromosome_)?group\.\w+?)?.*?"
+            )
+        )
+
+      match = filenames_regex.match(file_name)
+      species = match.group("species")
+      content_type = match.group("content_type")
+      if not release and format != "fasta":
+        release = match.group("release")
+    else:
+      species = path_array[-2]
+
+    if content_type:
+      content_type = '{"content_type":"'+content_type+'"}'
+
+    return format, species.lower(), content_type
