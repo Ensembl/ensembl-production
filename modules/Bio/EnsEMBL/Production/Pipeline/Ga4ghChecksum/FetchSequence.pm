533838622316ecc86cfe79d4019c94007748dea7
vinay <vinay@ebi.ac.uk>
2023-02-21 16:39:26 +0000

    base checksum pipeline setup

---
+++ modules/Bio/EnsEMBL/Production/Pipeline/Ga4ghChecksum/FetchSequence.pm
@@ -0,0 +1,162 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2023] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+=pod
+
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+=head1 NAME
+
+Bio::EnsEMBL::Production::Pipeline::Ga4ghChecksum::FetchSequence
+
+=head1 DESCRIPTION
+
+Fetch the toplevel genomic sequences  chromosomes, super contigs  and other sequence cDNA, CDS and Peptide
+
+
+=over 8 
+
+=item 1 - Fetch Toplevel sequences
+
+=item 2 - Fetch cDNA, CDS and Peptide sequences
+
+=back
+
+The script is responsible for genomic sequences for given sequence type
+
+Allowed parameters are:
+
+=over 8
+
+=item species - The species to fetch the genomic sequence
+
+=item sequence_type - The data to cetch. T<toplevel>, I<dna>, I<cdna> and I<pep> are allowed
+
+=item release - A required parameter for the version of Ensembl we are dumping for
+
+=item db_types - Array reference of the database groups to use. Defaults to core
+
+=back
+
+=cut
+
+package Bio::EnsEMBL::Production::Pipeline::Ga4ghChecksum::FetchSequence;
+
+use strict;
+use warnings;
+
+use base qw/Bio::EnsEMBL::Production::Pipeline::Common::Base/;
+
+my $DNA_INDEXING_FLOW = 1;
+my $PEPTIDE_INDEXING_FLOW = 2;
+my $GENE_INDEXING_FLOW = 3;
+my $CDS_INDEXING_FLOW = 4;
+
+sub param_defaults {
+  my ($self) = @_;
+  return {
+    #user configurable
+    allow_appending => 1,
+    overwrite_files => 0,
+    
+    dna_chunk_size => 17000,
+    
+    skip_logic_names => [],
+    process_logic_names => [],
+    
+    #DON'T MESS
+    #used to track if we need to reopen a file in append mode or not
+    generated_files => {}, 
+    remove_files_from_dir => {},
+    dataflows => [],
+    process_logic_active => 0,
+    skip_logic_active => 0,
+  };
+}
+
+sub fetch_input {
+  my ($self) = @_;
+  my %sequence_types = map { $_ => 1 } @{ $self->param('sequence_type') };
+  $self->param('sequence_types', \%sequence_types);
+ 
+  
+  my $types = $self->param('db_types');
+  $types = ['core'] unless $types;
+  $self->param('db_types', $types);
+
+  return;
+}
+
+sub run {
+  my ($self) = @_;
+  my $types = $self->param('db_types');
+  my $dba = $self->get_DBAdaptor($type);
+    if(! $dba) {
+      $self->info("Cannot continue with %s as we cannot find a DBAdaptor", $type);
+      next;
+    }
+    #get all toplevel sequences
+    #my ($chr, $non_chr, $non_ref) = $self->get_slices($dba);
+    #$self->print_to_file($chr, 'chr', $sm_filename, '>', $repeat_analyses);
+    my $sa = $dba->get_SliceAdaptor();
+    my @slices = @{ $sa->fetch_all( 'toplevel', undef, 1, undef, undef ) };
+    foreach my $slice (@slices) {
+      print($slice->seq())
+      print($slice->coord_system()->name())
+      print($slice->seq_region_name();)
+    }
+#   foreach my $type (@{$types}) {
+    
+
+
+
+#     #https://www.ensembl.org/info/docs/api/core/core_tutorial.html#genes
+#     ## The spliced_seq() method returns the concatenation of the exon
+#     # sequences. This is the cDNA of the transcript
+#     #print "cDNA: ", $transcript->spliced_seq(), "\n";
+
+# # The translateable_seq() method returns only the CDS of the transcript
+# #print "CDS: ", $transcript->translateable_seq(), "\n";
+
+#     #$self->run_type($type);
+# #     my $transcript_adaptor =
+# #   $registry->get_adaptor( 'Human', 'Core', 'Transcript' );
+# # my $transcript = $transcript_adaptor->fetch_by_stable_id($stable_id);
+
+# # print $transcript->translation()->stable_id(), "\n";
+# # print $transcript->translate()->seq(),         "\n";
+
+# # print $transcript->translation()->transcript()->stable_id(), "\n";
+
+
+#   }
+  return;
+}
+
+
+
+1;
+
---
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/Ga4gh_conf.pm
@@ -0,0 +1,130 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2023] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package Bio::EnsEMBL::Production::Pipeline::PipeConfig::Ga4gh_conf;
+
+use strict;
+use warnings;
+
+use base ('Bio::EnsEMBL::Production::Pipeline::PipeConfig::Base_conf');
+
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+use Bio::EnsEMBL::Hive::Version 2.5;
+use File::Spec;
+
+sub default_options {
+    my ($self) = @_;
+    return {
+        %{$self->SUPER::default_options()},
+
+        ## General parameters
+        'release'                => $self->o('ensembl_release'),
+        'pipeline_name'          => "ga4gh_checksum_generator_".$self->o('ensembl_release'),
+        'web_email'              => '',
+        'sequence_types'         => [],
+        ## 'job_factory' parameters
+        'species'                => [],
+        'antispecies'            => [],
+        'division'               => [],
+        'dbname'                 => undef,
+    };
+}
+
+sub pipeline_create_commands {
+  my ($self) = @_;
+
+  return [
+    @{$self->SUPER::pipeline_create_commands}
+  ];
+}
+
+# Ensures output parameters gets propagated implicitly
+sub hive_meta_table {
+  my ($self) = @_;
+
+  return {
+    %{$self->SUPER::hive_meta_table},
+    'hive_use_param_stack' => 1,
+  };
+}
+
+sub pipeline_wide_parameters {
+  my ($self) = @_;
+
+  return {
+    %{$self->SUPER::pipeline_wide_parameters},
+    'pipeline_name' => $self->o('pipeline_name'),
+    'release'       => $self->o('release'),
+  };
+}
+
+sub resource_classes {
+  my ($self) = @_;
+
+  return {
+    %{$self->SUPER::resource_classes},
+     '64GB' => { 'LSF' => '-q '.$self->o('production_queue').' -M  64000 -R "rusage[mem=64000]"' },
+    '128GB' => { 'LSF' => '-q '.$self->o('production_queue').' -M 128000 -R "rusage[mem=128000]"' },
+    '256GB' => { 'LSF' => '-q '.$self->o('production_queue').' -M 256000 -R "rusage[mem=256000]"' },
+  }
+}
+
+sub pipeline_analyses {
+    my ($self) = @_;
+
+    my $pipeline_flow;
+    my $sequence_types = $self->o('sequence_types');
+    my @sequence_types = (ref($sequence_types) eq 'ARRAY') ? @$sequence_types : ($sequence_types);
+
+    if (scalar @sequence_types) {
+        $pipeline_flow = $sequence_types;
+    } else {
+        $pipeline_flow = [ 'update_seqregion_attrib_toplevel', 'update_transcript_attrib_cdna',  'update_translation_attrib_pep' ]; #todo 'update_transcript_attrib_cds',
+    }
+
+    return [
+        { -logic_name        => 'job_factory',
+            -module          => 'Bio::EnsEMBL::Production::Pipeline::Common::SpeciesFactory',
+            -parameters      => {
+                species     => $self->o('species'),
+                antispecies => $self->o('antispecies'),
+                division    => $self->o('division'),
+                dbname      => $self->o('dbname'),
+                run_all     => $self->o('run_all'),
+            },
+            -input_ids       => [ {} ],
+            -hive_capacity   => -1,
+            -max_retry_count => 1,
+            -flow_into       => { '2' => 'backbone_job_pipeline', },
+        },
+        { -logic_name      => 'backbone_job_pipeline',
+            -module        => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -hive_capacity => -1,
+            -flow_into => ['FetchSequence']
+        },
+        { -logic_name      => 'fetch_toplevel_seq',
+            -module        => 'Bio::EnsEMBL::Production::Pipeline::Ga4ghChecksum::FetchSequence',
+            -hive_capacity => -1,
+        },
+
+    ];
+}
+
+1;
+
