26e9f3d8690eed4a05a60d93ae5f363f778c8951
vinay-ebi <vinay@ebi.ac.uk>
2023-02-22 09:29:41 +0000

    toplevel sequence testing

--- modules/Bio/EnsEMBL/Production/Pipeline/Ga4ghChecksum/FetchSequence.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/Ga4ghChecksum/FetchSequence.pm
@@ -99,8 +99,8 @@ sub param_defaults {
 
 sub fetch_input {
   my ($self) = @_;
-  my %sequence_types = map { $_ => 1 } @{ $self->param('sequence_type') };
-  $self->param('sequence_types', \%sequence_types);
+  #my %sequence_types = map { $_ => 1 } @{ $self->param('sequence_type') };
+  #$self->param('sequence_types', \%sequence_types);
   my $types = $self->param('db_types');
   $types = ['core'] unless $types;
   $self->param('db_types', $types);
@@ -110,7 +110,7 @@ sub fetch_input {
 
 sub run {
   my ($self) = @_;
-  my $types = $self->param('db_types');
+  #my $types = $self->param('db_types');
   my $type = 'core';
   my $dba = $self->get_DBAdaptor($type);
     if(! $dba) {
@@ -123,9 +123,10 @@ sub run {
     my $sa = $dba->get_SliceAdaptor();
     my @slices = @{ $sa->fetch_all( 'toplevel', undef, 1, undef, undef ) };
     foreach my $slice (@slices) {
-      print($slice->seq());
-      print($slice->coord_system()->name());
-      print($slice->seq_region_name());
+	    #print($slice->seq());
+      print($dba->dbc->dbname,"\n");
+      print($slice->coord_system()->name(), "\n");
+      print($slice->seq_region_name(), "\n");
     }
 #   foreach my $type (@{$types}) {
     
