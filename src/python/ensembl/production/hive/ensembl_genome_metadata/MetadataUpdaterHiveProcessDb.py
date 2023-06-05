2462bbd84317b94e2e9027ae07eead3894c5bf4d
Daniel Poppleton <111403332+dpopleton@users.noreply.github.com>
2023-06-05 10:26:44 +0100

    Features/genome metadata hive (#797)
    * Updates in assembly-converter
    - single source of data for a release
    - added options
    - changed paths
    
    * Added Slurm configurations
    
    * Added Slurm configurations
    
    * New Metadata Implementations
    
    * Added metadata to requirments
    
    * Fixed Hive flow
    
    * Fixed Hive flow
    
    * Fixed Hive flow
    
    * Updated Paramater inputs
    
    * Updated Paramater inputs
    
    * Updates in assembly-converter
    - single source of data for a release
    - added options
    - changed paths
    
    * Added Slurm configurations
    
    * Added Slurm configurations
    
    * New Metadata Implementations
    
    * Added metadata to requirments
    
    * Fixed Hive flow
    
    * Fixed Hive flow
    
    * Fixed Hive flow
    
    * Updated Paramater inputs
    
    * Updated Paramater inputs
    
    * Revert "Added Slurm configurations"
    
    This reverts commit 5b9b8e68
    
    * Revert "Fixed Hive flow"
    
    This reverts commit d7d30e0ec771e2a03d74164f4dc7306fbf10dccd.
    
    * Revert "Added Slurm configurations"
    
    This reverts commit 5b9b8e68
    
    * Updated Paramater inputs
    
    * Revert "Updates in assembly-converter"
    
    This reverts commit fb2f07da9218e8906da20bc62e9c82b64fb6cbd6.
    
    * Improvments for PR
    
    * Improvments for PR
    
    * Update src/python/ensembl/production/hive/ensembl_genome_metadata/MetadataUpdaterHiveProcessDb.py
    
    Co-authored-by: Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
    
    * code cleanup
    
    * Update src/python/ensembl/production/hive/ensembl_genome_metadata/MetadataUpdaterHiveProcessDb.py
    
    Co-authored-by: Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
    
    * Update src/python/ensembl/production/hive/ensembl_genome_metadata/MetadataUpdaterHiveProcessDb.py
    
    Co-authored-by: Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
    
    * Hive for metadata
    
    * Nextflow Slurm configuration
    
    * removed variation and funcgen from metadataupdater pipeline
    
    * Update requirements.in
    
    Co-authored-by: Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
    
    ---------
    
    Co-authored-by: Marc Chakiachvili <mchakiachvili@ebi.ac.uk>

---
+++ src/python/ensembl/production/hive/ensembl_genome_metadata/MetadataUpdaterHiveProcessDb.py
@@ -0,0 +1,60 @@
+#  See the NOTICE file distributed with this work for additional information
+#  regarding copyright ownership.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from pathlib import Path
+import re
+import json
+from ensembl.production.hive.BaseProdRunnable import BaseProdRunnable
+from eHive import ParamException
+from sqlalchemy.engine.url import make_url
+
+
+class MetadataUpdaterHiveProcessDb(BaseProdRunnable):
+    def fetch_input(self):
+        try:
+            payload = json.loads(self.param_required("payload"))
+            self.param("comment", payload["input"]["comment"])
+            self.param("database_uri", payload["input"]["database_uri"])
+            self.param_required("database_uri")
+            self.param("email", payload["input"]["email"])
+            self.param("metadata_uri", payload["input"]["metadata_uri"])
+            self.param_required("metadata_uri")
+            self.param("source", payload["input"]["source"])
+            self.param("timestamp", payload["input"]["timestamp"])
+        except json.JSONDecodeError as e:
+            raise ParamException(e)
+
+    def run(self):
+        db_url = make_url(self.param("database_uri"))
+        output = {
+            "database_uri": self.param("database_uri"),
+            "metadata_uri": self.param("metadata_uri"),
+        }
+        if '_compara_' in db_url.database:
+            self.dataflow(output, 4)
+        elif '_core_' in db_url.database:
+            self.dataflow(output, 3)
+        elif '_otherfeatures_' in db_url.database:
+            self.dataflow(output, 7)
+        elif '_rnaseq_' in db_url.database:
+            self.dataflow(output, 8)
+        elif '_cdna_' in db_url.database:
+            self.dataflow(output, 9)
+        # Dealing with other versionned databases like mart, ontology,...
+        elif re.match('^\w+_?\d*_\d+$', db_url.database):
+            self.dataflow(output, 10)
+        elif re.match(
+                '^ensembl_accounts|ensembl_archive|ensembl_autocomplete|ensembl_production|ensembl_stable_ids|ncbi_taxonomy|ontology|website',
+                db_url.database):
+            self.dataflow(output, 10)
+        else:
+            raise Exception("Can't find data_type for database " + db_url.database)
