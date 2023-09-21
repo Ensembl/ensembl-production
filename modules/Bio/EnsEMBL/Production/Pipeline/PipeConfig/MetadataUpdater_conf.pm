ec512be00430283210bb61b8f7f1bfb3b9e83ea7
danielp <danielp@ebi.ac.uk>
2023-09-21 16:34:36 +0100

    small updates

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/MetadataUpdater_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/MetadataUpdater_conf.pm
@@ -26,8 +26,7 @@ use warnings;
 use Data::Dumper;
 
 use base ('Bio::EnsEMBL::Production::Pipeline::PipeConfig::Base_conf');
-use Bio::EnsEMBL::Hive::Version 2.5;
-use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+
 
 
 sub default_options {
