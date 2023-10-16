9f73e08382bb868e932600947fce66e76b42f262
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2023-10-16 12:11:16 +0100

    Updated `organisms` path to issue from scientific name instead of display name.

--- modules/Bio/EnsEMBL/Production/Pipeline/FileDump/Base.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/FileDump/Base.pm
@@ -101,10 +101,14 @@ sub species_name {
   $self->throw("Missing dba parameter: species_name method") unless defined $dba;
 
   my $mca = $dba->get_adaptor("MetaContainer");
-  my $species_name = $mca->single_value_by_key('species.display_name');
+  my $species_name = $mca->single_value_by_key('species.scientific_name');
   if (defined $species_name and $species_name ne '') {
-    $species_name =~ s/^([\w ]+) [\-\(].+/$1/;
-    $species_name =~ s/ /_/g;
+    # Replace non letter chars from string
+    $species_name =~ s/[^a-zA-Z0-9]+/ /g;
+    # Replace consecutive spaces with a single underscore
+    $species_name =~ s/ +/_/g;
+    # Remove leading and trailing underscores (if any)
+    $species_name =~ s/^_+|_+$//g;
   } else {
     $self->throw("No species.display_name");
   }
