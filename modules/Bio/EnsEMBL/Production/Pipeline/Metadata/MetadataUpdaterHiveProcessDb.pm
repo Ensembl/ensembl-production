4f4e238944cd67478b4a2f67677b3bc0d04b5d40
vinay-ebi <59567245+vinay-ebi@users.noreply.github.com>
2024-02-07 16:22:55 +0000

    fix copy right issue in MetadataUpdater

--- modules/Bio/EnsEMBL/Production/Pipeline/Metadata/MetadataUpdaterHiveProcessDb.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/Metadata/MetadataUpdaterHiveProcessDb.pm
@@ -1,5 +1,6 @@
 =head1 LICENSE
 
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 Copyright [2016-2024] EMBL-European Bioinformatics Institute
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +14,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+=cut
 
 =head1 NAME
  Bio::EnsEMBL::MetaData::Pipeline::MetadataUpdaterHiveProcessDb;
