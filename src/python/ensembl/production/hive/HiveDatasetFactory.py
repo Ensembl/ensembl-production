b9e93aaca8b9b3fad8dcdf36e1b26365669296d6
vinay-ebi <vinay@ebi.ac.uk>
2024-02-17 20:37:09 +0000

    hive dataset factory added

---
+++ src/python/ensembl/production/hive/HiveDatasetFactory.py
@@ -0,0 +1,55 @@
+#  See the NOTICE file distributed with this work for additional information
+#  regarding copyright ownership.
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""
+Hive DatasetFactory module to perform CRUD operation on dataset 
+"""
+
+import eHive
+from ensembl.production.metadata.api.hive.dataset_factory import DatasetFactory
+
+class HiveDatasetFactory(eHive.BaseRunnable, DatasetFactory):
+  
+  def __init__(self, read_fileno, write_fileno, debug):
+    super().__init__(read_fileno, write_fileno, debug)
+    DatasetFactory.__init__(self, metadata_uri=self.param_required('metadata_db_uri') )
+  
+  def fetch_input(self):
+    #set default request method to update_dataset_status and set default params to update the dataset status
+    self.request_methods =  ['update_dataset_status'] if self.param('request_methods') is None else self.param('request_methods')
+    self.request_method_params =   {
+      'update_dataset_status' : { 'dataset_uuid': self.param('dataset_uuid'), 'status': self.param('dataset_status')},
+    } if  self.param('request_method_params') is None  else self.param('request_method_params')    
+      
+      
+  def dispatch_request(self,request, **kwargs):
+    method = getattr(super(), request, None)
+    if method and callable(method):
+        method(**kwargs.get('params'))
+    else:
+        raise ValueError(f"Invalid request method {method} ")
+
+  def run(self):
+    try:
+      for request_method in self.request_methods:    
+        response = self.dispatch_request(request_method, params=self.request_method_params[request_method])
+        self.dataflow(
+          { request_method: response}, 2  
+        )
+        print(response)
+    except KeyError as e:
+        raise KeyError(f"Missing request parameters: {str(e)}")
+    except Exception as e:
+        raise Exception(f"An unexpected error occurred: {str(e)}")
+
+  def write_output(self):
+    pass
