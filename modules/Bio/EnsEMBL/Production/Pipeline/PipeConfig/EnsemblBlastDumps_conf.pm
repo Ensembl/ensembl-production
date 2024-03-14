3b62dd4e0dd1469669e29294dba5892a17c14859
vinay-ebi <vinay@ebi.ac.uk>
2024-03-14 13:51:21 +0000

    dedicated blast pipeline for new site

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/EnsemblBlastDumps_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/EnsemblBlastDumps_conf.pm
@@ -21,171 +21,28 @@ package Bio::EnsEMBL::Production::Pipeline::PipeConfig::EnsemblBlastDumps_conf;
 
 use strict;
 use warnings;
-
-use base ('Bio::EnsEMBL::Production::Pipeline::PipeConfig::Base_conf');
-use Bio::EnsEMBL::Production::Pipeline::PipeConfig::DumpCore_conf;
-use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
-use Bio::EnsEMBL::Hive::Version 2.5;
-use File::Spec::Functions qw(catdir);
-
+use base ('Bio::EnsEMBL::Production::Pipeline::PipeConfig::BlastFileDump_conf');
 
 sub default_options {
   my ($self) = @_;
   return {
     %{$self->SUPER::default_options},
+
+    genome_types => [
+      'Genome_FASTA',
+    ],
+    geneset_types => [
+      'Geneset_FASTA',
+    ],
+    rnaseq_types => [],
+
+    dump_metadata => 1,
+
+    blast_index => 1,
     
-    #load default from mysql dumps
-    %{Bio::EnsEMBL::Production::Pipeline::PipeConfig::DumpCore_conf::default_options($self)},
-
-    skip_convert_fasta => 1,
-    #registry 
-    registry          => $self->o('registry'),
-    # Metadata Report Script
-    species           => [],
-    division          => [],
-    base_dir          => $self->o('ENV', 'BASE_DIR'),
-    ensembl_version   => $self->o('ENV', 'ENS_VERSION'),
-    eg_version        => $self->o('ENV', 'EG_VERSION'),
-    division_list     => ['vertebrates','fungi','metazoa','plants','protists'],  #'bacteria'  
-
-    ## fasta parameters
-    # types to emit
-    dna_sequence_type_list => [ 'dna' ],
-    pep_sequence_type_list => [ 'cdna', 'ncrna' ],
-    prev_rel_dir           => '/nfs/ftp/ensemblftp/ensembl/PUBLIC/pub/',
-
-    #Mysql Dumps Params
-    # Database type factory
-    group => ['core'],
-
-    # By default, files are not written if the parent directory
-    # (which is the database name) exists.
-    overwrite => 0,
-    dump_dir  => undef,
+    run_datachecks   => 1,
+    datacheck_groups => ['rapid_release'],
   };
 }
-
-# Implicit parameter propagation throughout the pipeline.
-sub hive_meta_table {
-  my ($self) = @_;
-
-  return {
-    %{$self->SUPER::hive_meta_table},
-    'hive_use_param_stack' => 1,
-  };
-}
-
-sub pipeline_wide_parameters {
-  my ($self) = @_;
-
-  return {
-    %{$self->SUPER::pipeline_wide_parameters},
-     username        => $self->o('username'),
-     dump_dir        => $self->o('dump_dir') 
-  };
-}
-
-
-sub pipeline_create_commands {
-    my ($self) = @_;
-    return [
-        @{$self->SUPER::pipeline_create_commands},
-        'mkdir -p ' . $self->o('dump_dir'),
-    ];
-}
-
-sub pipeline_analyses {
-  my $self = shift @_;
-
-  return [  
-
-    @{Bio::EnsEMBL::Production::Pipeline::PipeConfig::Base_conf::pipeline_analyses($self)},  
-    {
-      -logic_name        => 'SpeciesFactory',
-      -module            => 'Bio::EnsEMBL::Production::Pipeline::Common::SpeciesFactory',
-      -max_retry_count   => 1,
-      -analysis_capacity => 20,
-      -flow_into         => {
-        '2' => ['Dumps']
-      }
-        
-    },
-  
-    { 
-      -logic_name        => 'Dumps',
-      -module            => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
-      -flow_into         => { '1->A' => ['fasta_pep', 'fasta_dna'], 'A->1' => 'checksum_generator' },
-    },
-    {
-      -logic_name        => 'Email',
-      -module            => 'Bio::EnsEMBL::DataCheck::Pipeline::EmailNotify',
-      -max_retry_count   => 1,
-      -analysis_capacity => 10,
-      -parameters        => {
-        email         => $self->o('email'),
-        pipeline_name => $self->o('pipeline_name'),
-      },
-    },
-    {
-      -logic_name      => 'checksum_generator',
-      -module        => 'Bio::EnsEMBL::Production::Pipeline::Common::ChksumGenerator',
-      -parameters    => {
-        dumps              => ['fasta_pep', 'fasta_dna'],
-        # skip_convert_fasta => '#expr( #dump_division# eq "vertebrates" ? 1 : 0 )expr#',  
-      },
-      -hive_capacity => 10,
-    },
-    {
-     -logic_name      => 'fasta_pep',
-     -module          => 'Bio::EnsEMBL::Production::Pipeline::FASTA::DumpFile',
-     -parameters      => {
-        sequence_type_list  => ['cdna','ncrna'],
-        process_logic_names => $self->o('process_logic_names'),
-        skip_logic_names    => $self->o('skip_logic_names'),
-        base_path           => '#base_path#',
-      },
-      -can_be_empty    => 1,
-      -max_retry_count => 1,
-      -hive_capacity   => 20,
-      -priority        => 5,
-      -rc_name         => '2GB',
-    },
-    { 
-      -logic_name      => 'fasta_dna',
-      -module          => 'Bio::EnsEMBL::Production::Pipeline::FASTA::DumpFile',
-      -parameters      => {
-        sequence_type_list  => $self->o('dna_sequence_type_list'),
-        process_logic_names => $self->o('process_logic_names'),
-        skip_logic_names    => $self->o('skip_logic_names'),
-       },
-      -can_be_empty    => 1,
-      -flow_into       => { 1 => 'concat_fasta' },
-      -max_retry_count => 1,
-      -hive_capacity   => 20,
-      -priority        => 5,
-      -rc_name         => '4GB',
-    },
-   
-    { 
-      -logic_name      => 'concat_fasta',
-      -module          => 'Bio::EnsEMBL::Production::Pipeline::FASTA::ConcatFiles',
-      -can_be_empty    => 1,
-      -parameters      => {
-        blat_species => $self->o('blat_species'),
-      },
-      -max_retry_count => 5,
-      -priority        => 5,
-      -flow_into         => WHEN('#dump_division# eq "vertebrates"' => ['primary_assembly'])
-    },
-    {
-      -logic_name      => 'primary_assembly',
-      -module          => 'Bio::EnsEMBL::Production::Pipeline::FASTA::CreatePrimaryAssembly',
-      -can_be_empty    => 1,
-      -max_retry_count => 5,
-      -priority        => 5,
-    },	
-
-  ];
-} 
 
 1;
