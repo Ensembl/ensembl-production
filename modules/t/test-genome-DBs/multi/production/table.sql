153c7d6aefa25698acf68dba07fcd6f11ab5410b
vinay <vinay@ebi.ac.uk>
2024-09-22 20:28:28 +0100

    set travis dist to bionic to revert mysql version to 5.7 similar to production staging server

--- modules/t/test-genome-DBs/multi/production/table.sql
+++ modules/t/test-genome-DBs/multi/production/table.sql
@@ -13,7 +13,7 @@ CREATE TABLE `analysis_description` (
   `displayable` tinyint(1) NOT NULL,
   PRIMARY KEY (`analysis_description_id`),
   UNIQUE KEY `logic_name_idx` (`logic_name`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `master_attrib` (
   `attrib_id` int(11) unsigned NOT NULL DEFAULT '0',
@@ -26,7 +26,7 @@ CREATE TABLE `master_attrib` (
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`attrib_id`),
   UNIQUE KEY `type_val_idx` (`attrib_type_id`,`value`(80))
-) ENGINE=InnoDB ;
+) ENGINE=MyISAM ;
 
 CREATE TABLE `master_attrib_set` (
   `attrib_set_id` int(11) unsigned NOT NULL DEFAULT '0',
@@ -38,7 +38,7 @@ CREATE TABLE `master_attrib_set` (
   `modified_at` datetime DEFAULT NULL,
   UNIQUE KEY `set_idx` (`attrib_set_id`,`attrib_id`),
   KEY `attrib_idx` (`attrib_id`)
-) ENGINE=InnoDB ;
+) ENGINE=MyISAM ;
 
 CREATE TABLE `master_attrib_type` (
   `attrib_type_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
@@ -52,7 +52,7 @@ CREATE TABLE `master_attrib_type` (
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`attrib_type_id`),
   UNIQUE KEY `code_idx` (`code`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `master_biotype` (
   `biotype_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -72,12 +72,12 @@ CREATE TABLE `master_biotype` (
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`biotype_id`),
   UNIQUE KEY `name_type_idx` (`name`,`object_type`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `master_external_db` (
   `external_db_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `db_name` varchar(100) NOT NULL,
-  `db_release` varchar(100) DEFAULT NULL,
+  `db_release` varchar(255) DEFAULT NULL,
   `status` enum('KNOWNXREF','KNOWN','XREF','PRED','ORTH','PSEUDO') NOT NULL,
   `priority` int(11) NOT NULL,
   `db_display_name` varchar(255) NOT NULL,
@@ -92,7 +92,7 @@ CREATE TABLE `master_external_db` (
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`external_db_id`),
   UNIQUE KEY `db_name_idx` (`db_name`,`db_release`,`is_current`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `master_misc_set` (
   `misc_set_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
@@ -107,7 +107,7 @@ CREATE TABLE `master_misc_set` (
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`misc_set_id`),
   UNIQUE KEY `code_idx` (`code`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `master_unmapped_reason` (
   `unmapped_reason_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -119,7 +119,7 @@ CREATE TABLE `master_unmapped_reason` (
   `modified_by` int(11) DEFAULT NULL,
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`unmapped_reason_id`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `meta` (
   `meta_id` int(11) NOT NULL AUTO_INCREMENT,
@@ -129,7 +129,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`(255)),
   KEY `species_value_idx` (`species_id`,`meta_value`(255))
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `meta_key` (
   `meta_key_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -145,7 +145,7 @@ CREATE TABLE `meta_key` (
   `is_multi_value` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`meta_key_id`),
   KEY `name_type_idx` (`name`,`db_type`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE TABLE `web_data` (
   `web_data_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -157,7 +157,7 @@ CREATE TABLE `web_data` (
   `modified_at` datetime DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`web_data_id`)
-) ENGINE=InnoDB  ;
+) ENGINE=MyISAM  ;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=CURRENT_USER() SQL SECURITY INVOKER VIEW `attrib` AS select `master_attrib`.`attrib_id` AS `attrib_id`,`master_attrib`.`attrib_type_id` AS `attrib_type_id`,`master_attrib`.`value` AS `value` from `master_attrib` where (`master_attrib`.`is_current` = 1) order by `master_attrib`.`attrib_id`;
 
