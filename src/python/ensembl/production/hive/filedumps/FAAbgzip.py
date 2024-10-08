cf370519de1880fdac7e4d91fd0043a3050247e6
danielp <danielp@ebi.ac.uk>
2024-10-08 16:01:33 +0100

    Minor fix to flow

--- src/python/ensembl/production/hive/filedumps/FAAbgzip.py
+++ src/python/ensembl/production/hive/filedumps/FAAbgzip.py
@@ -27,6 +27,7 @@ class FAAbgzip(eHive.BaseRunnable):
         # This is total garbage. We should not be creating the six jobs to begin with, but I am done with this for now.
         if "softmasked.fa" not in output_filename:
             self.dataflow({'attribute_dict': {}, 'trigger_next_step': 0}, 2)
+            return
         softmasked_filename = str(Path(output_filename).parent / "softmasked.fa")
         # Split the path into parts and find the index of "organisms"
         path_parts = Path(softmasked_filename).parts
