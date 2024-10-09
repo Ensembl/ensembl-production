f544f5d6a6aad7bf4a45e31a3734745dc0bf8ef5
danielp <danielp@ebi.ac.uk>
2024-10-09 13:26:31 +0100

    Minor fix to flow

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/FileDump_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/FileDump_conf.pm
@@ -293,27 +293,30 @@ sub pipeline_analyses {
 
             },
             -rc_name         => '4GB',
-            -flow_into       => [
-                               'FAAbgzip'
-            ],
+            -flow_into       => {
+                '-1'   => [ 'Genome_FASTA_mem' ],
+               '2->A' => [ 'FAAbgzip' ],
+                'A->1' => [ 'Compress_File' ],
+            },
         },
-        # {
-        #     -logic_name      => 'Genome_FASTA_mem',
-        #     -module          => 'Bio::EnsEMBL::Production::Pipeline::FileDump::Genome_FASTA',
-        #     -max_retry_count => 1,
-        #     -hive_capacity   => 10,
-        #     -parameters      => {
-        #         blast_index    => 0,
-        #         per_chromosome => 0,
-        #         overwrite      => 1,
-        #         unmasked       => 1,
-        #         hardmasked     => 1,
-        #     },
-        #     -rc_name         => '8GB',
-        #     -flow_into       => {
-        #        '2->A' => [ 'FAAbgzip' ],
-        #     },
-        # },
+        {
+            -logic_name      => 'Genome_FASTA_mem',
+            -module          => 'Bio::EnsEMBL::Production::Pipeline::FileDump::Genome_FASTA',
+            -max_retry_count => 1,
+            -hive_capacity   => 10,
+            -parameters      => {
+                blast_index    => 0,
+                per_chromosome => 0,
+                overwrite      => 1,
+                unmasked       => 1,
+                hardmasked     => 1,
+            },
+            -rc_name         => '8GB',
+            -flow_into       => {
+               '2->A' => [ 'FAAbgzip' ],
+               'A->1' => [ 'Compress_File' ],
+            },
+        },
         {
             -logic_name      => 'Chromosome_TSV',
             -module          => 'Bio::EnsEMBL::Production::Pipeline::FileDump::Chromosome_TSV',
