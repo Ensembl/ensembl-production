ce0ed9046f0b3fce712f7b2200d77cb368fb30c2
vinay <vinay@ebi.ac.uk>
2025-03-31 19:54:07 +0100

    fix geneset fasta

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/FileDumpMVP_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/FileDumpMVP_conf.pm
@@ -41,7 +41,7 @@ sub default_options {
         dump_dir                           => undef,
         ftp_root                           => undef,
         genome_types                       => [ 'Assembly_Chain', 'Chromosome_TSV', 'Genome_FASTA' ],
-        geneset_types                      => [ 'Geneset_EMBL' ], #,  'Geneset_FASTA', 'Geneset_GFF3', 'Geneset_GTF', 'Xref_TSV' ],
+        geneset_types                      => [ 'Geneset_EMBL' ,  'Geneset_FASTA'],  #, 'Geneset_GFF3', 'Geneset_GTF', 'Xref_TSV' ],
         homology_types                     => [ 'Homologies_TSV' ], # Possible values :
 
         overwrite                          => 0,
@@ -369,6 +369,34 @@ sub pipeline_analyses {
             -parameters      => {
                 per_chromosome => 0,
                 overwrite      => 1,
+            },
+            -rc_name         => '4GB',
+            -flow_into       => {
+                '2' => [ 'Compress_File' ]
+            },
+        },
+        {
+            -logic_name      => 'Geneset_FASTA',
+            -module          => 'Bio::EnsEMBL::Production::Pipeline::FileDump::Geneset_FASTA',
+            -max_retry_count => 1,
+            -hive_capacity   => 10,
+            -parameters      => {
+                blast_index => 0,
+            },
+            -rc_name         => '1GB',
+            -flow_into       => {
+                '-1' => [ 'Geneset_FASTA_mem' ],
+                '2'  => [ 'Compress_File' ]
+            },
+        },
+        {
+            -logic_name      => 'Geneset_FASTA_mem',
+            -module          => 'Bio::EnsEMBL::Production::Pipeline::FileDump::Geneset_FASTA',
+            -max_retry_count => 1,
+            -hive_capacity   => 10,
+            -parameters      => {
+                blast_index => 0,
+                overwrite   => 1,
             },
             -rc_name         => '4GB',
             -flow_into       => {
