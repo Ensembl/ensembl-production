08cd66fe86519b7bea82f896f4b02d2d80a12468
vinay <vinay@ebi.ac.uk>
2025-04-16 09:39:35 +0100

    Split protein config change

--- modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/ProteinFeaturesMVP_RunInterpro_conf.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/PipeConfig/ProteinFeaturesMVP_RunInterpro_conf.pm
@@ -17,7 +17,7 @@ limitations under the License.
 
 =cut
 
-package Bio::EnsEMBL::Production::Pipeline::PipeConfig::ProteinFeatures_conf;
+package Bio::EnsEMBL::Production::Pipeline::PipeConfig::ProteinFeaturesMVP_Runinterpro_conf;
 
 use strict;
 use warnings;
